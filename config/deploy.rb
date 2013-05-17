bfc558eb79a9b501d7727e4a429c2cef1eb035f4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-05-17 11:08:33 -0400

    Updating :scm_command to shared path

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ ssh_options[:paranoid] = false
 
 set :scm, :git
 set :deploy_via, :remote_cache
-set :scm_command, '/usr/bin/git'
+set :scm_command, '/shared/git/bin/git'
 
 #############################################################
 #  Environment
