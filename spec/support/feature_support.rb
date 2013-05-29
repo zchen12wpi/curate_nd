100e6fb844f4fc6df46869d55b1316867ce5f948
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-05-29 11:16:07 -0400

    Switching to poltergeist driver
    With the update to Firefox, my webdriver was not working. This required
    an update for JS related tests.

--- spec/support/feature_support.rb
+++ spec/support/feature_support.rb
@@ -1,6 +1,22 @@
 module FeatureSupport
+  def with_javascript?
+    @example.metadata[:js] || @example.metadata[:javascript]
+  end
+
   def login_as(user)
     user.reload # because the user isn't re-queried via Warden
     super(user, scope: :user, run_callbacks: false)
   end
+
+  module_function
+  def options(default = {type: :feature})
+    if ENV['JS']
+      Capybara.javascript_driver = default.fetch(:javascript_driver, :poltergeist_debug)
+      default[:js] = true
+    else
+      Capybara.javascript_driver = default.fetch(:javascript_driver, :poltergeist)
+    end
+    default
+  end
+
 end
