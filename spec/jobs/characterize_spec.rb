7bb0d56b44ad99bb2f2add44c3d4b3fc486a258a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-16 09:03:25 -0400

    Adding more debugging for characterization

--- spec/jobs/characterize_spec.rb
+++ spec/jobs/characterize_spec.rb
@@ -2,32 +2,21 @@ require 'spec_helper'
 
 describe CharacterizeJob do
   let(:user) { FactoryGirl.create(:user) }
+  let(:image_file_path) {
+    File.expand_path('../../fixtures/files/image.png', __FILE__)
+  }
   let(:image_file) {
-    Rack::Test::UploadedFile.new(
-      File.expand_path('../../fixtures/files/image.png', __FILE__),
-      'image/png',
-      false
-    )
+    Rack::Test::UploadedFile.new(image_file_path,'image/png',false)
   }
   let(:generic_file) { FactoryGirl.create_generic_file(:senior_thesis, user, image_file ) }
 
   subject { CharacterizeJob.new(generic_file.pid) }
 
   it 'should use create a thumbnail' do
-    begin
-      subject.run
-    rescue Exception => e
-      file = GenericFile.find(generic_file.pid)
-      $stderr.puts "*" * 80
-      $stderr.puts "pid: #{generic_file.pid}"
-      $stderr.puts "mime_type: #{file.mime_type.inspect}"
-      $stderr.puts "format_label: #{file.format_label.inspect}"
-      $stderr.puts "-" * 80
-      $stderr.puts e
-      $stderr.puts "-" * 80
-      $stderr.puts e.backtrace.join("\n")
-      $stderr.puts "*" * 80
-      raise e
-    end
+    subject.run
+  end
+
+  it 'checking characterization runner' do
+    expect(FileContentDatastream.new.run_fits!(image_file_path)).to eq("")
   end
 end
