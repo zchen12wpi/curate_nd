61238a4ac08943f55af448655d7e9244e02e77c4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-18 09:35:58 -0400

    Removing deprecation warnings

--- spec/repository_models/person_spec.rb
+++ spec/repository_models/person_spec.rb
@@ -6,13 +6,13 @@ describe Person do
     @user = User.new
     @user.username= "jsmith@example.com"
     @user.save
-    @user.stub!(:email).and_return("jsmith.test@example.com")
-    @user.stub!(:display_name).and_return("John Smith")
+    @user.stub(:email).and_return("jsmith.test@example.com")
+    @user.stub(:display_name).and_return("John Smith")
     @user
   }
 
   describe '.find_or_create_by_user' do
-    it 'should create a new person object' do 
+    it 'should create a new person object' do
       obtained_result = Person.find_or_create_by_user(user)
       obtained_result.class.should eq Person
     end
