a69ddfe2e1968e619e5b103540177a652e78752a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-25 09:47:13 -0400

    Making cap-task.sh executible

--- script/cap-task.sh
+++ script/cap-task.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
 # Invoked by the CurateND-Integration Jenkins project
-#
-echo "=-=-=-=-=-=-=-= start $0 $1 $2"
+echo "=-=-=-=-=-=-=-= start $0 $1 $@"
 
-local target_env=$1
-local capistrano_command=$2
+target_env=$1
+shift 1
+capistrano_command=$@
 
 if [ -z $target_env ]; then
     echo "=-=-=-=-=-=-=-= target_env parameter missing"
-    echo "Exiting do_deploy"
-    return 1
+    echo "Exiting $0"
+    exit 1
 fi
-echo "=-=-=-=-=-=-=-= environment $target_env"
+echo "=-=-=-=-=-=-=-= environment: ${target_env}"
 
-if [ -z $capistrano_command ]; then
+if [ -z "$capistrano_command" ]; then
     echo "=-=-=-=-=-=-=-= capistrano_command parameter missing"
-    echo "Exiting do_deploy"
-    return 1
+    echo "Exiting $0"
+    exit 1
 fi
+
+echo "=-=-=-=-=-=-=-= command: ${capistrano_command}"
 
 LANG=en_US.UTF-8
 LC_ALL=en_US.UTF-8
@@ -33,6 +35,9 @@ echo "=-=-=-=-=-=-=-= bundle install"
 echo "=-=-=-=-=-=-=-= cd $WORKSPACE"
 cd $WORKSPACE
 
-# echo "=-=-=-=-=-=-=-= cap ${target_env} deploy"
-# $WORKSPACE/vendor/bundle/bin/cap -v -f "$WORKSPACE/Capfile" ${target_env}_cluster deploy
-# retval=$?
+echo "=-=-=-=-=-=-=-= cap ${target_env}_cluster ${capistrano_command}"
+$WORKSPACE/vendor/bundle/bin/cap -v -f "$WORKSPACE/Capfile" ${target_env}_cluster $capistrano_command
+retval=$?
+
+echo "=-=-=-=-=-=-=-= do_deploy finished $retval"
+exit $retval
