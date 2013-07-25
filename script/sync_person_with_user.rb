01fd2ea95107d7ad5310105d7697425dd370ab2c
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-07-25 11:48:15 -0400

    Adding logging to update person status

--- script/sync_person_with_user.rb
+++ script/sync_person_with_user.rb
@@ -1,8 +1,16 @@
-class SyncPersonWithUser
-  def self.sync
+module SyncPersonWithUser
+  module_function
+  def sync
+    @exceptions = []
     User.find_each do |user|
-      Person.find_or_create_by_user(user)
+      begin
+        Person.find_or_create_by_user(user)
+      rescue LdapService::UserNotFoundError => e
+        @failures << e.to_s
+      end
     end
+    $stderr.put(@exceptions.join("\n"))
+    logger.error(@exceptions.join("\n"))
   end
 end
 
