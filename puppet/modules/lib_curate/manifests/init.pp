66e920c43e514f3d6abf9b58a9cb07555629106c
Don Brower <dbrower@nd.edu>
2013-08-08 14:07:06 -0400

    Add more packages for puppet

--- puppet/modules/lib_curate/manifests/init.pp
+++ puppet/modules/lib_curate/manifests/init.pp
@@ -4,7 +4,26 @@ class lib_curate {
 
 	# Binary package requirements
 	
-	$packagelist = [ "clamav" , "clamav-db", "clamav-devel", "ImageMagick", "GConf2", "ORBit2", "libIDL", "libcroco", "libgsf", "librsvg2", "libtool-ltdl", "libwmf-lite", "libxml2-devel", "sgml-common", "htop", "tmux" ]
+	$packagelist = [
+    "clamav",
+    "clamav-db",
+    "clamav-devel",
+    "ImageMagick",
+    "GConf2",
+    "ORBit2",
+    "libIDL",
+    "libcroco",
+    "libgcrpyt-devel",
+    "libgpg-error-devel",
+    "libgsf",
+    "librsvg2",
+    "libtool-ltdl",
+    "libwmf-lite",
+    "libxml2-devel",
+    "libxslt-devel",
+    "sgml-common",
+    "htop",
+    "tmux" ]
 
 	package { $packagelist:
         	ensure => installed
