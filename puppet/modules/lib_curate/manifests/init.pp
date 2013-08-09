6e6eac6dbb1e9c3ba3db8c8efa1fbfcdebd03a28
msuhovec <suhovecky@nd.edu>
2013-08-09 08:26:05 -0400

    Added mysql:config, lib_fits, lib_redis, lib_tomcat6, lib_fedora
    Plaus the hiera lookups needed to configure all this stuff

--- puppet/modules/lib_curate/manifests/init.pp
+++ puppet/modules/lib_curate/manifests/init.pp
@@ -1,8 +1,28 @@
-# Set up the Common Pacakges needed for CurateND
 
 class lib_curate {
 
-	# Binary package requirements
+    include mysql
+
+   # lookup data we need to build this node
+   $fedora_admin_mysql = hiera('fedora_admin_mysql')
+   $fedora_db_name = hiera('fedora_db_name')
+   $fedora_passwd = hiera('fedora_passwd')
+   $fedora_passwd_mysql = hiera('fedora_passwd_mysql')
+   $fedora_root = hiera('fedora_root')
+   $fedora_version = hiera('fedora_version')
+   $mysql_db_name = hiera('mysql_db_name')
+   $mysql_root_password = hiera('mysql_root_password')
+   $solr_conf_dir = hiera('solr_conf_dir')
+   $solr_corename = hiera('solr_corename')
+   $solr_data_dir = hiera('solr_data_dir')
+   $solr_instance_dir = hiera('solr_instance_dir')
+   $solr_root = hiera('solr_root')
+   $solr_version = hiera('solr_version')
+   $tomcat_config_dir = hiera('tomcat_config_dir')
+   $tomcat_root = hiera('tomcat_root')
+
+   # Set up the Common Pacakges needed for CurateND
+   # Binary package requirements
 	
 	$packagelist = [
     "clamav",
@@ -30,5 +50,50 @@ class lib_curate {
         	ensure => installed
 	}
 
-  include mysql
+     # Install FITS
+     class { 'lib_fits':
+	require => Package[$packagelist],
+     }	
+
+
+     # Install REDIS
+     class { 'lib_redis':
+	require => Package[$packagelist],
+     }	
+
+     # Install SSL Certs
+     class { 'lib_certs':
+	require => Package[$packagelist],
+     }	
+
+     # Install and Configure mysql for fedora
+     
+     class { 'mysql::config':
+	root_password => $mysql_root_password,
+	require => Package[$packagelist],
+     }	
+
+     mysql::db { "${fedora_db_name}":
+	user => $fedora_admin_mysql,
+	password => $fedora_passwd_mysql,
+	host => 'localhost',
+	grant => ['all'].
+	require => Class['mysql::config'],
+     }
+
+     #Install and start Tomcat6
+     class { 'lib_tomcat6':
+	require => Package[$packagelist],
+     }	
+
+     # Install Fedora after Tomcat and mysql set up
+     class { 'lib_fedora':
+	require => Class["lib_tomcat6"], Mysql:Db["${fedora_db_name}"],
+     }
+
+     # Install SOLR after Fedora- tell tomcat
+     class { 'lib_solr':
+	require => Class["lib_fedora"],
+     }
+
 }
