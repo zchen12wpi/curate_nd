b6b77e30e03c5b3945cf6d39c8f11b14bc0ec57a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-04 16:26:41 -0400

    Removing merge conflict

--- config/deploy.rb
+++ config/deploy.rb
@@ -245,12 +245,12 @@ set :repository,  "git://github.com/ndlib/curate_nd.git"
 
 desc "Setup for staging VM"
 task :staging do
-  set :branch,    'vm-deploy'
+  set :branch,    'master'
   set :rails_env, 'staging'
   set :deploy_to, '/home/app/curatend'
   set :user,      'app'
   set :domain,    fetch(:host, 'libvirt6.library.nd.edu')
-  set :bundle_without,  [:development, :test]
+  set :bundle_without, [:development, :test]
   set :shared_directories,  %w(log)
   set :shared_files, %w()
 
@@ -325,11 +325,7 @@ def common_worker_things
   set :scm_command, '/usr/bin/git'
   set :deploy_to,   '/home/curatend'
   set :ruby_bin,    '/usr/local/ruby/bin'
-<<<<<<< HEAD
-  set :bundle_without,  [:development, :test]
-=======
   set :bundle_without, [:development, :test]
->>>>>>> "Moving" bundle_without to master.
   set :group_writable, false
 
   default_environment['PATH'] = "#{ruby_bin}:$PATH"
