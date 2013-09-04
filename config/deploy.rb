937ebebdb3011d958be2f8880e996cc246f5d565
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-09-04 16:22:00 -0400

    Adding bundle_without for workers

--- config/deploy.rb
+++ config/deploy.rb
@@ -319,7 +319,7 @@ def common_worker_things
   set :scm_command, '/usr/bin/git'
   set :deploy_to,   '/home/curatend'
   set :ruby_bin,    '/usr/local/ruby/bin'
-  set :without_bundle_environments, 'development test'
+  set :bundle_without,  [:development, :test]
   set :group_writable, false
 
   default_environment['PATH'] = "#{ruby_bin}:$PATH"
