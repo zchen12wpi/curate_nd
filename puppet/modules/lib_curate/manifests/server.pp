c308c055dea8a60e4a7b01bfdad47f17935b0ca8
msuhovec <suhovecky@nd.edu>
2013-09-24 11:49:49 -0400

    Initial standalone, worker, server.pp files in lib_curate for pprd/prod deploys

---
+++ puppet/modules/lib_curate/manifests/server.pp
@@ -0,0 +1,68 @@
+
+class lib_curate::server {
+
+   # lookup data we need to build this node
+
+   # Set up the Common Pacakges needed for CurateND
+   # Binary package requirements
+	
+	$packagelist = [
+    "clamav",
+    "clamav-db",
+    "clamav-devel",
+    "ImageMagick",
+    "GConf2",
+    "ORBit2",
+    "libIDL",
+    "libcroco",
+    "libgcrypt-devel",
+    "libgpg-error-devel",
+    "libgsf",
+    "librsvg2",
+    "libtool-ltdl",
+    "libwmf-lite",
+    "libxml2-devel",
+    "libxslt-devel",
+    "readline-devel",
+    "mysql-devel",
+    "sgml-common",
+    "htop",
+    "tmux" ]
+
+	package { $packagelist:
+        	ensure => installed
+	}
+
+
+     # Install REDIS
+     class { 'lib_redis':
+	require => Package[$packagelist],
+     }	
+
+     # Install SSL Certs
+     class { 'lib_certs':
+	require => Package[$packagelist],
+     }	
+
+     # Install app user
+     class { 'lib_app_home':
+	require => Package[$packagelist],
+     }	
+
+     # Install Ruby
+     class { 'lib_ruby':
+	require => Package[$packagelist],
+     }	
+
+     class { 'lib_nginx':
+	require => Class["lib_certs"],
+     }
+
+     file { '/etc/nginx/conf.d/curatend.conf':
+       content => template('lib_curate/curatend.conf.erb'),
+       owner => 'root',
+       group => 'root',
+       mode => '644',
+       notify => Service['nginx'],
+     }
+}
---
+++ puppet/modules/lib_curate/manifests/worker.pp
@@ -0,0 +1,60 @@
+
+class lib_curate::worker {
+
+   # Set up the Common Pacakges needed for CurateND
+   # Binary package requirements
+	
+	$packagelist = [
+    "clamav",
+    "clamav-db",
+    "clamav-devel",
+    "ImageMagick",
+    "GConf2",
+    "ORBit2",
+    "libIDL",
+    "libcroco",
+    "libgcrypt-devel",
+    "libgpg-error-devel",
+    "libgsf",
+    "librsvg2",
+    "libtool-ltdl",
+    "libwmf-lite",
+    "libxml2-devel",
+    "libxslt-devel",
+    "readline-devel",
+    "mysql-devel",
+    "sgml-common",
+    "htop",
+    "tmux" ]
+
+	package { $packagelist:
+        	ensure => installed
+	}
+
+     # Install FITS
+     class { 'lib_fits':
+	require => Package[$packagelist],
+     }	
+
+
+     # Install REDIS
+     class { 'lib_redis':
+	require => Package[$packagelist],
+     }	
+
+     # Install app user
+     class { 'lib_app_home':
+	require => Package[$packagelist],
+     }	
+
+     # Install Ruby
+     class { 'lib_ruby':
+	require => Package[$packagelist],
+     }	
+
+
+     # Install resque worker daemon and service
+     class { 'lib_resque_poold':
+	require => Class["lib_ruby"],
+     }
+}
