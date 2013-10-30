d2890487c25da2344cd48846564b89abdb47999f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-10-30 16:30:06 -0400

    Adding UserSignupBehavior pseudo-code

---
+++ spec/features/user_signup_behavior.rb
@@ -0,0 +1,16 @@
+require 'spec_helper_features'
+
+describe 'user profile behavior', FeatureSupport.options do
+  context 'new user' do
+    it 'behaves as expected' do
+      # go to homepage
+      # click login
+      # accept terms of service
+      # update my profile
+      # upload a new dataset
+      # then I add a section to my profile
+      # and I add the dataset to my profile
+      # and for extra credit my profile does not show up in search results
+    end
+  end
+end
