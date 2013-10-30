64896dbe30de3c42425b1a96a03f6c50791ab6cd
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-10-30 11:48:52 -0400

    Tidying up against current state of Curate
    * This adds migrations from Curate's engine
    * Removes the VALID_CURATION_CONCERN_CLASS_NAMES which are now
    registered.
    * Removes _form which caused problems with underlying Curate works
    
    Related to ndlib/curate#240
    Related to #314

--- app/models/classify_concern.rb
+++ app/models/classify_concern.rb
@@ -1,12 +1,6 @@
 require Curate::Engine.root.join('app/models/classify_concern')
 class ClassifyConcern
-  VALID_CURATION_CONCERN_CLASS_NAMES = [
-    'SeniorThesis',
-    'Article',
-    'Dataset'
-  ]
   UPCOMING_CONCERNS = [
-    ['Image', 'Deposite photographs or generated images.'],
     ['Video', 'Upload your video files.']
   ]
 end
