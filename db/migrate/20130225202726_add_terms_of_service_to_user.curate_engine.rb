2bc81ab1a3bc92685df85d9ab0ce9a323b210ef4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-10-30 15:50:28 -0400

    Correcting migration behavior

--- db/migrate/20130225202726_add_terms_of_service_to_user.curate_engine.rb
+++ db/migrate/20130225202726_add_terms_of_service_to_user.curate_engine.rb
@@ -1,4 +1,4 @@
-class AddUserAgreedToTermsOfService < ActiveRecord::Migration
+class AddTermsOfServiceToUser < ActiveRecord::Migration
   def change
     add_column :users, :agreed_to_terms_of_service, :boolean, default: false
   end
