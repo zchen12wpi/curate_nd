438b14da04b93b205582cfa91e44abd8c23085c9
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2013-11-13 15:32:00 -0500

    Updating the schema by running the migrations

--- db/schema.rb
+++ db/schema.rb
@@ -253,12 +253,6 @@ ActiveRecord::Schema.define(version: 20131111204848) do
 
   add_index "trophies", ["user_id"], name: "index_trophies_on_user_id", using: :btree
 
-  create_table "user_whitelists", force: true do |t|
-    t.string "username"
-  end
-
-  add_index "user_whitelists", ["username"], name: "index_user_whitelists_on_username", unique: true, using: :btree
-
   create_table "users", force: true do |t|
     t.string   "email",                                default: "",    null: false
     t.string   "encrypted_password",                   default: "",    null: false
