553ff7984561bd4335149b919631f049f59aa7a7
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2013-11-22 08:53:50 -0500

    Adding a placeholder about page
    Update links in the site footer
    
    Static page for about
    Enables the completion of ndlib/planning#360
    
    Link to the about page in the footer

--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,6 @@ CurateNd::Application.routes.draw do
   end
   devise_for :users, controllers: { sessions: :sessions, registrations: :registrations }, skip: :masquerades
 
+  get 'about', to: 'static_pages#about'
 
 end
