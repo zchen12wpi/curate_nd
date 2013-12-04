e9003dfc0fe014aa5eed819735ed13d203078b32
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-12-04 15:15:18 -0500

    Hot fix for curation_concern_person_path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def attribute_to_html(curation_concern, method_name, options = {})
-    label = curation_concern.label_for(method_name)
-    curation_concern_attribute_to_html(curation_concern, method_name, label, options)
-  end
 
   def curation_concern_collection_path(asset)
     collection_path(asset)
+  end
+
+  def curation_concern_person_path(person)
+    person_path(person)
   end
 end
