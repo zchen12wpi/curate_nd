265ecac5082af21fd6047d4cb8e6d0d7e68c0286
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-12-04 15:15:18 -0500

    Hot fix for curation_concern_person_path

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,20 +1,14 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe '#attribute_to_html' do
-    subject { double('curation_concern', things: collection) }
-    let(:collection) { ["<h2>", "Johnny Tables"] }
-    it 'uses the label for when one is given' do
-      label = 'Children'
-      subject.stub(:label_for).with(:things).and_return(label)
-      rendered = helper.attribute_to_html(subject, :things)
-      rendered.should have_tag('tr') do
-        with_tag("th", text: label)
-        with_tag('td ul.tabular') do
-          with_tag('li.attribute.things', text: '<h2>')
-          with_tag('li.attribute.things', text: 'Johnny Tables')
-        end
-      end
+  context '#curation_concern_collection_path' do
+    it 'should use #collection_path' do
+      expect(curation_concern_collection_path(id: '1234')).to eq('/collections/1234')
+    end
+  end
+  context '#curation_concern_person_path' do
+    it 'should use #person_path' do
+      expect(curation_concern_person_path(id: '1234')).to eq('/people/1234')
     end
   end
 end
