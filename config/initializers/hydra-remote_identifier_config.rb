031a8528e529027c7d0d5a995271849b2eece413
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-05-14 15:58:35 -0400

    Updating doi mapping

--- config/initializers/hydra-remote_identifier_config.rb
+++ config/initializers/hydra-remote_identifier_config.rb
@@ -11,9 +11,18 @@ Hydra::RemoteIdentifier.configure do |config|
       map.set_identifier {|o,value| o.identifier = value; o.save }
     end
 
-    doi.register(Article, Dataset, Image, Document) do |map|
+    doi.register(Dataset, Image, Document) do |map|
       map.target {|obj| Curate.permanent_url_for(obj) }
       map.creator {|obj| Array.wrap(obj.contributor).collect(&:to_s).join(", ") }
+      map.title :title
+      map.publisher {|o| Array.wrap(o.publisher).join("; ")}
+      map.publicationyear {|o| o.date_uploaded.year }
+      map.set_identifier {|o,value| o.identifier = value; o.save }
+    end
+
+    doi.register(Article) do |map|
+      map.target {|obj| Curate.permanent_url_for(obj) }
+      map.creator {|obj| Array.wrap(obj.creator).collect(&:to_s).join(", ") }
       map.title :title
       map.publisher {|o| Array.wrap(o.publisher).join("; ")}
       map.publicationyear {|o| o.date_uploaded.year }
