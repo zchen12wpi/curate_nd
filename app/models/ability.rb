367c5a55c8b7eefa170759438c9e50bd080f299d
Rajesh Balekai <rbalekai@nd.edu>
2014-06-23 14:57:30 -0400

    Permissions for Finding Aid

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,6 +5,18 @@ class Ability
 
   # Define any customized permissions here.
   def custom_permissions
+    @work_type_permissions ||= WorkTypePermissions.new(current_user)
+
+    Curate.configuration.registered_curation_concern_types.each do |work_type|
+      #The access is controlled by the work_type_permission.yml file
+      #For a work, all the registered users will be given create access
+      #unless a group id is specified instead of 'all' in the yml file.
+      if !@work_type_permissions.is_permitted?(work_type)
+        #Only concerned with work creation.
+        #Edit can be controlled by access permissions settings for an object
+        cannot :create, work_type.constantize
+      end
+    end
     # Limits deleting objects to a the admin user
     #
     # if current_user.admin?
