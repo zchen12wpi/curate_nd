04bec0160871859f3a6ad01348ec5f95bd491980
Rajesh Balekai <rbalekai@nd.edu>
2014-06-23 14:57:30 -0400

    Permissions for Finding Aid

---
+++ app/models/work_type_permissions.rb
@@ -0,0 +1,19 @@
+class WorkTypePermissions
+  def initialize( current_user ) 
+    @user = current_user
+  end
+
+  def is_permitted?( work_type )
+    begin
+      work_type_permission = WORK_PERMISSIONS.fetch( work_type.to_s )
+      open_to = work_type_permission.fetch( 'open' ) { 'nobody' }
+      case open_to
+        when 'all' then true
+        when 'nobody' then false
+        else @user.groups.include?( group_id )
+      end
+    rescue KeyError
+      false
+    end
+  end
+end
