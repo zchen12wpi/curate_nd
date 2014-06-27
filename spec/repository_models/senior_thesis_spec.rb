e0a2e9aa3feafe48ddd96c051a14201a5880b6f2
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-06-27 13:12:13 -0400

    Fixing CurateND DOI test

--- spec/repository_models/senior_thesis_spec.rb
+++ spec/repository_models/senior_thesis_spec.rb
@@ -10,6 +10,5 @@ describe SeniorThesis do
   it_behaves_like 'is_a_curation_concern_model'
   it_behaves_like 'with_access_rights'
   it_behaves_like 'is_embargoable'
-  it_behaves_like 'remotely_identified', :doi
 
 end
