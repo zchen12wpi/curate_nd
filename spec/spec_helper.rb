64543b94a3ebdc5a291d72f7838baaa015fd7c87
Don Brower <dbrower@nd.edu>
2014-07-31 17:30:59 -0400

    Use noids server as identity service

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -71,6 +71,12 @@ RSpec.configure do |config|
 
   config.before(:all) do
     WebMock.allow_net_connect!
+    VCR.configure do |c|
+      c.cassette_library_dir = "spec/fixtures/cassettes"
+      c.hook_into :webmock
+      c.ignore_localhost = false
+      c.allow_http_connections_when_no_cassette = true
+    end
   end
 
   config.before(:each) do
