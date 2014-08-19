d2180696722fb0097b0d78304a323ed661cde9a3
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-19 11:27:29 -0400

    Adding harbinger migrations
    $ rake harbinger:install:migrations

---
+++ db/migrate/20140819133853_create_harbinger_database_channel_message.harbinger.rb
@@ -0,0 +1,14 @@
+# This migration comes from harbinger (originally 20140310185338)
+class CreateHarbingerDatabaseChannelMessage < ActiveRecord::Migration
+  def change
+    create_table :harbinger_messages do |t|
+      t.string :reporters
+      t.string :state, limit: 32
+      t.integer :message_object_id
+      t.timestamps
+    end
+    add_index :harbinger_messages, :state
+    add_index :harbinger_messages, :reporters
+    add_index :harbinger_messages, :message_object_id
+  end
+end
