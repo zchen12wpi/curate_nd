2460ead52f33c5801c6ddce5a4fe055c455f3023
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-08-19 11:45:22 -0400

    Modifying migrations to reflect errors encountered

--- db/migrate/20140819133853_create_harbinger_database_channel_message.harbinger.rb
+++ db/migrate/20140819133853_create_harbinger_database_channel_message.harbinger.rb
@@ -4,7 +4,7 @@ class CreateHarbingerDatabaseChannelMessage < ActiveRecord::Migration
     create_table :harbinger_messages do |t|
       t.string :reporters
       t.string :state, limit: 32
-      t.integer :message_object_id
+      t.string :message_object_id
       t.timestamps
     end
     add_index :harbinger_messages, :state
