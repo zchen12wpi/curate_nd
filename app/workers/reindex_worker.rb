0ea88c4d407d122f5729778c13f493b58e6dd470
Don Brower <dbrower@nd.edu>
2014-09-26 14:01:14 -0400

    Add route to pass ids of objects to be indexed
    The route acepts a JSON array of the object ids to reindex.
    (either the full fedora PID or just the NOID portion).
    The reindexing happens as a bunch of background jobs, each job handling
    up to 10 ids each.
    
    The route is behind a new constraint which requires an API key.
    There is a config file for API keys, and it is added to the deploy.
    (hence future deploys may break until the file is added to the secrets
    repos).
    
    Addresses DLTP-117

--- app/workers/reindex_worker.rb
+++ app/workers/reindex_worker.rb
@@ -3,7 +3,19 @@ class ReindexWorker
     :resolrize
   end
 
+  attr_reader :pids_to_reindex
+
+  def initialize(pid_list = nil)
+    @pids_to_reindex = pid_list.nil? ? :everything : Array.wrap(pid_list)
+  end
+
   def run
-    ActiveFedora::Base.reindex_everything("pid~#{Sufia.config.id_namespace}:*")
+    if @pids_to_reindex == :everything
+      ActiveFedora::Base.reindex_everything("pid~#{Sufia.config.id_namespace}:*")
+    else
+      @pids_to_reindex.each do |pid|
+        ActiveFedora::Base.find(pid, cast: true).update_index
+      end
+    end
   end
 end
