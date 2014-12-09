ea1aabfe2b7d3b6f7e87d8ef2f9df8df29900be5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-12-09 08:58:58 -0500

    Adjusting rendering options for SimpleForm
    By changing the form builder from `use :input` imperative, I'm instead
    wrapping the input call with the aria-labelledby and aria-describedby.
    
    This is not a bullet-proof solution but delivers on what was
    requested.
    
    The included test is for a reference implementation and may not be
    reflective of the full test that we are after.
    
    References DLTP-324

---
+++ spec/views/shared/_simple_form_input_test.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'shared/simple_form_input_test.html.erb' do
+  let(:object) { SeniorThesis.new }
+  let(:user) { User.new }
+  before(:each) do
+    controller.stub(:current_user).and_return(user)
+    assign(:object, object)
+    render partial: 'simple_form_input_test'
+  end
+  it 'should have Cancel link which takes to root page' do
+    expect(rendered).to have_tag('.control-group.senior_thesis_title') do
+      with_tag('.control-label') do
+        with_tag('label[for="senior_thesis_title"]') do
+          with_tag('#senior_thesis_title_label', text: 'Title')
+        end
+        with_tag('.field-hint#senior_thesis_title_hint[role="tooltip"]')
+      end
+      with_tag('.controls') do
+        with_tag('input[aria-required="true"][name="senior_thesis[title]"][aria-labelledby="senior_thesis_title_label"][aria-describedby="senior_thesis_title_hint"]')
+      end
+    end
+  end
+end
