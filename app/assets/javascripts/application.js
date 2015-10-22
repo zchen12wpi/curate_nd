c87fba15e5d43917640c7dfdfbeebd9d74230dc3
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2015-10-22 10:13:00 -0400

    Break out the tile view JavaScript into a module
    The project needs a better JavaScript encapsulation strategy.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,56 +62,6 @@ $(function(){
     $(this).next('.field-hint').slideToggle(100);
   });
 
-  // Tile search results menu (TSRM)
-  $('.tile-menu-toggle').on('click', function(e) {
-    e.preventDefault();
-    $('.tile-actions-menu.focus').removeClass('focus');
-    $(this).parent('.tile-actions-menu').toggleClass('focus');
-    e.stopPropagation();
-  });
-
-  // TSRM: Save mouse-wielding users having to click
-  $('.tile-menu-toggle').on('mouseover', function(e) {
-    $('.tile-actions-menu.focus').removeClass('focus');
-    $(this).parent('.tile-actions-menu').addClass('focus');
-  });
-
-  // TSRM: Catch-all for dismissing open menus
-  $('.page-main').on('click', function(e) {
-    $('.tile-actions-menu.focus').removeClass('focus');
-  });
-
-  // TSRM: Turn on tile display
-  $('.search .choose-list-format .grid').on('click', function(e) {
-    if($(this).hasClass('active')){
-      e.preventDefault();
-    }
-    else {
-      e.preventDefault();
-      if(window.location.search === ''){
-        window.location.href = window.location.href + '?display=grid';
-      } else {
-        window.location.href = window.location.href + '&display=grid';
-      }
-    }
-  });
-
-  // TSRM: Turn off tile display
-  $('.search .choose-list-format .listing').on('click', function(e) {
-    if($(this).hasClass('active')){
-      e.preventDefault();
-    }
-    else {
-      e.preventDefault();
-      var url = window.location.href.split('?'),
-          params = url[1].split('&'),
-          nonDisplayParams = params.filter(function(param){return param.split('=')[0] !== 'display'}),
-          target = url[0] + '?' + nonDisplayParams.join('&');
-
-      window.location.href = target;
-    }
-  });
-
   $('.field-hint')
     .hide()
     .before(
