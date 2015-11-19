10f3b768c23277ec7d1c2be578e5d599e92de754
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2015-11-19 09:33:31 -0500

    Updating tests to reflect time-bound access tokens

--- spec/models/temporary_access_token_spec.rb
+++ spec/models/temporary_access_token_spec.rb
@@ -11,17 +11,17 @@ describe TemporaryAccessToken do
   end
 
   context 'when being created' do
-    it 'permits a null sha during object initialization' do
+    it 'permits a null SHA during object initialization' do
       token = FactoryGirl.build(:temporary_access_token, sha: nil)
       expect(token.sha).to eq(nil)
     end
 
-    it 'sets a sha during object persistance' do
+    it 'sets a SHA during object persistence' do
       token = FactoryGirl.create(:temporary_access_token, sha: nil)
       expect(token.sha).not_to eq(nil)
     end
 
-    it 'ignores sha values passed during object initialization' do
+    it 'ignores SHA values passed during object initialization' do
       provided_sha = 'please_ignore_this_sha'
       token = FactoryGirl.create(:temporary_access_token, sha: provided_sha)
       expect(token.sha).not_to eq(provided_sha)
@@ -35,38 +35,45 @@ describe TemporaryAccessToken do
     end
   end
 
-    context 'when validating existing tokens' do
+  context 'when validating existing tokens' do
     subject { FactoryGirl.create(:temporary_access_token, attributes) }
 
-    it 'should allow access exisiting, unused tokens' do
+    it 'should allow access to existing, unused tokens' do
       subject.save!
       expect(TemporaryAccessToken.count).not_to eq(0)
       expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(true)
     end
 
-    it 'should not allow access exisiting, used tokens' do
-      subject.used = true
+    it 'should set a expiry date the first time it has been used' do
       subject.save!
-      expect(TemporaryAccessToken.count).not_to eq(0)
-      expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(false)
+      expect(subject.expiry_date).to be_nil
+      expect(TemporaryAccessToken.use!(subject.sha)).to eq(true)
+      expect(subject.expiry_date).to_not be_nil
     end
 
-    it 'should not allow access to a token after it has been expired' do
+    it 'should not change the expiry date if the token is used repeatedly' do
+      expected_expiry_date = Time.now + 1.hour
+      subject.expiry_date = expected_expiry_date
       subject.save!
-      expect(TemporaryAccessToken.count).not_to eq(0)
+      expect(TemporaryAccessToken.use!(subject.sha)).to eq(false)
+      expect(subject.expiry_date).to eq(expected_expiry_date)
+    end
+
+    it 'should permit access with a token to be used repeatedly before the expiry period has elapsed' do
+      subject.expiry_date = Time.now + 1.hour
+      subject.save!
       expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(true)
-      expect(TemporaryAccessToken.expire!(subject.sha)).to eq(true)
+      expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(true)
+    end
+
+    it 'should not permit access with a token after the expiry period has elapsed' do
+      subject.expiry_date = Time.now - 1.hour
+      subject.save!
       expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(false)
     end
 
-    it 'should not be able to expire a used token' do
-      subject.used = true
-      subject.save!
-      expect(TemporaryAccessToken.expire!(subject.sha)).to eq(false)
-    end
-
-    it 'should not be able to expire a token with an unknown sha' do
-      expect(TemporaryAccessToken.expire!('sha_that_does_not_exist')).to eq(false)
+    it 'should not be able to update the expiry date of a token with an unknown sha' do
+      expect(TemporaryAccessToken.use!('sha_that_does_not_exist')).to eq(false)
     end
   end
 end
