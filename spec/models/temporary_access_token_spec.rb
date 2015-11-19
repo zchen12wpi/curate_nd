7124419447a227fff3b0278131941758f70c6562
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2015-11-19 13:11:45 -0500

    Use described_class convention in unit tests

--- spec/models/temporary_access_token_spec.rb
+++ spec/models/temporary_access_token_spec.rb
@@ -40,16 +40,16 @@ describe TemporaryAccessToken do
 
     it 'should allow access to existing, unused tokens' do
       subject.save!
-      expect(TemporaryAccessToken.count).not_to eq(0)
+      expect(described_class.count).not_to eq(0)
       expect(subject.expiry_date).to be_nil
-      expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(true)
+      expect(described_class.permitted?(subject.noid, subject.sha)).to eq(true)
     end
 
     it 'should set a expiry date the first time it has been used' do
       subject.save!
       expect(subject.expiry_date).to be_nil
-      expect(TemporaryAccessToken.use!(subject.sha)).to eq(true)
-      updated_token = TemporaryAccessToken.find(subject.sha)
+      expect(described_class.use!(subject.sha)).to eq(true)
+      updated_token = described_class.find(subject.sha)
       expect(updated_token.expiry_date).to_not be_nil
     end
 
@@ -57,25 +57,25 @@ describe TemporaryAccessToken do
       expected_expiry_date = Time.now + 1.hour
       subject.expiry_date = expected_expiry_date
       subject.save!
-      expect(TemporaryAccessToken.use!(subject.sha)).to eq(false)
+      expect(described_class.use!(subject.sha)).to eq(false)
       expect(subject.expiry_date).to eq(expected_expiry_date)
     end
 
     it 'should permit access with a token to be used repeatedly before the expiry period has elapsed' do
       subject.expiry_date = Time.now + 1.hour
       subject.save!
-      expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(true)
-      expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(true)
+      expect(described_class.permitted?(subject.noid, subject.sha)).to eq(true)
+      expect(described_class.permitted?(subject.noid, subject.sha)).to eq(true)
     end
 
     it 'should not permit access with a token after the expiry period has elapsed' do
       subject.expiry_date = Time.now - 1.hour
       subject.save!
-      expect(TemporaryAccessToken.permitted?(subject.noid, subject.sha)).to eq(false)
+      expect(described_class.permitted?(subject.noid, subject.sha)).to eq(false)
     end
 
     it 'should not be able to update the expiry date of a token with an unknown sha' do
-      expect(TemporaryAccessToken.use!('sha_that_does_not_exist')).to eq(false)
+      expect(described_class.use!('sha_that_does_not_exist')).to eq(false)
     end
   end
 end
