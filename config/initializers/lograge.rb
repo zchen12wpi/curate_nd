0873dddacddaa3c0614c2dfdf1008cc4cc9f5f10
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2015-12-18 10:39:32 -0500

    Timestamps are added to logstash-logger

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -3,6 +3,6 @@ CurateNd::Application.configure do
   config.lograge.formatter = Lograge::Formatters::Logstash.new
   config.lograge.custom_options = lambda do |event|
     params = event.payload[:params].reject { |k| %w(controller action).include?(k) }
-    { params: params, time: event.time }
+    { params: params }
   end
 end
