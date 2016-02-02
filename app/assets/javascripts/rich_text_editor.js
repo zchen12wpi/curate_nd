7535e86d015e1840fcc52e0633c3ec39cdf3313d
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-02-02 14:15:36 -0500

    Including and configuring SimpleMDE

---
+++ app/assets/javascripts/rich_text_editor.js
@@ -0,0 +1,10 @@
+//= require simplemde.min.js
+
+$(function(){
+  'use strict';
+
+   var simplemde = new SimpleMDE({
+     element: $('.rich-textarea-js')[0],
+     hideIcons: ['image']
+   });
+});
