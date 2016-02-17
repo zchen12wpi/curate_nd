46e43e303647930fefb427f1bbf2bcaedc58f4f8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-17 15:47:46 -0500

    Updating downloads controller
    Forgot to include the < ApplicationController as I copied from
    the curate system.

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,6 @@
 require 'sufia/models/noid'
 
-class DownloadsController
+class DownloadsController < ApplicationController
   include Sufia::Noid # for normalize_identifier method
   include Hydra::Controller::DownloadBehavior
   prepend_before_filter :normalize_identifier, except: [:index, :new, :create]
