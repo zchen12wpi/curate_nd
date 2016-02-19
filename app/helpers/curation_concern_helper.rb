4626c69d3ba7faac579306ce66374c4c3d155493
Banurekha <banurekha.l@gmail.com>
2016-02-19 16:22:54 -0500

    Display “Department/Unit” on the show page of a work
    Adding metadat to show view
    Adding helper method to display administrative unit in breadcrumb style and related css

--- app/helpers/curation_concern_helper.rb
+++ app/helpers/curation_concern_helper.rb
@@ -3,4 +3,24 @@ module CurationConcernHelper
     options = args.extract_options!
     simple_form_for(object, *(args << options.merge(builder: CurationConcernFormBuilder)), &block)
   end
+
+  def decode_administrative_unit(curation_concern, method_name, label = nil, options = { class: 'descriptive-text' })
+    markup = ""
+    label ||= derived_label_for(curation_concern, method_name)
+    administrative_unit = curation_concern.public_send(method_name)
+    return markup if administrative_unit.blank?
+    markup << %(<tr><th>#{label}</th>\n<td><ul class='tabular'>)
+    [administrative_unit].flatten.compact.each do |value|
+      nodes = value.split("::")
+      markup << %(<ul class='breadcrumb'>)
+      markup << %(<li> <a href="#"> #{nodes[0]}</a></li>)
+      markup <<  %(<span class="divider">/</span>)
+      markup << %(<li> <a href="#"> #{nodes[1]}</a></li>)
+      markup <<  %(<span class="divider">/</span>)
+      markup << %(<li class="active"> #{nodes[2]}</li>)
+      markup << %(</ul>)
+    end
+    markup << %(</ul></td></tr>)
+    markup.html_safe
+  end
 end
