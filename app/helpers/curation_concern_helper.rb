fb8f06e7bf4a2f73b73161749d36b0a064b53899
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-02-23 10:23:19 -0500

    Removing links for Department and Units rendering
    Instead of links, prefer text. At a future point, we may want to
    include a link to a Facet.
    
    Also removing the assumption that the hierarchy is 3 deep. It can now
    support an arbitrary depth.

--- app/helpers/curation_concern_helper.rb
+++ app/helpers/curation_concern_helper.rb
@@ -10,15 +10,10 @@ module CurationConcernHelper
     administrative_unit = curation_concern.public_send(method_name)
     return markup if administrative_unit.blank?
     markup << %(<tr><th>#{label}</th>\n<td><ul class='tabular'>)
-    [administrative_unit].flatten.compact.each do |value|
-      nodes = value.split("::")
-      markup << %(<ul class='breadcrumb'>)
-      markup << %(<li> <a href="#"> #{nodes[0]}</a></li>)
-      markup <<  %(<span class="divider">/</span>)
-      markup << %(<li> <a href="#"> #{nodes[1]}</a></li>)
-      markup <<  %(<span class="divider">/</span>)
-      markup << %(<li class="active"> #{nodes[2]}</li>)
-      markup << %(</ul>)
+    [administrative_unit].flatten.compact.each do |hierarchy|
+      markup << %(<ul class='breadcrumb'><li>)
+      markup << hierarchy.split("::").join("</li><span class='divider'>/</span><li>")
+      markup << %(</ul></li>)
     end
     markup << %(</ul></td></tr>)
     markup.html_safe
