27a33e222f997cd59507dd3de48358bdd8c60a6d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-03-15 09:31:01 -0400

    Removing the freezing of Time via Timecop
    At one point, temporal control was vital. However that does not
    appear to be the case.

--- spec/spec_helper_features.rb
+++ spec/spec_helper_features.rb
@@ -3,7 +3,6 @@ require 'capybara/poltergeist'
 require 'compass-rails'
 require 'compass'
 require 'bootstrap-datepicker-rails'
-require 'timecop'
 
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
