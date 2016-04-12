9de02636b2a106cb236240f4dd5d437ff2562ff9
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-04-12 13:48:32 -0400

    Allow more than one group to have access to a work type

--- app/models/work_type_permissions.rb
+++ app/models/work_type_permissions.rb
@@ -1,6 +1,8 @@
 class WorkTypePermissions
+  attr_accessor :user
+
   def initialize( current_user )
-    @user = current_user
+    self.user = current_user
   end
 
   def is_permitted?( work_type )
@@ -10,10 +12,20 @@ class WorkTypePermissions
       case open_to
         when 'all' then true
         when 'nobody' then false
-        else @user.groups.include?( open_to )
+        else user_has_privileged_group?( open_to )
       end
     rescue KeyError
       false
     end
+  end
+
+  def user_groups
+    @user_groups ||= user.groups
+  end
+
+  def user_has_privileged_group?( group_keys )
+    privileged_groups = Array.wrap( group_keys )
+    users_privileged_groups = user_groups & privileged_groups
+    users_privileged_groups.any?
   end
 end
