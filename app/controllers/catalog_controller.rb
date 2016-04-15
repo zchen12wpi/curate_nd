83808b1029c0415b46d7e00312e75b511720fb48
Banurekha <banurekha.l@gmail.com>
2016-04-15 11:46:18 -0400

    Administrative unit Indexing strategy
    Adding two custom solr fields related to admin_unit. One is for stemmed, tokenized field with custom stop word.
    Other one to index whole hiearchy which could be used for future hierarchial faceting.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -274,7 +274,7 @@ class CatalogController < ApplicationController
       contributors = solr_name("contributors", :stored_searchable, type: :string)
       degree_department_acronyms = solr_name("degree_department_acronyms", :stored_searchable, type: :string)
       date_created = solr_name("desc_metadata__date_created", :stored_searchable, type: :string)
-      administrative_unit = solr_name("desc_metadata__administrative_unit", :stored_searchable, type: :string)
+      administrative_unit = "admin_unit_tesim"
       field.solr_parameters = {
         :qf => "#{title_name} #{label_name} noid_tsi file_format_tesim #{contributor_name} #{abstract_name} #{description_name} #{creator_name} #{administrative_unit} #{publisher_name} #{language_name} #{collection_name} #{contributor_institution_name} #{subject_name} #{identifier_name} #{urn} #{degree_name} #{degree_disciplines} #{contributors} #{degree_department_acronyms} #{date_created}",
         :pf => "#{title_name}"
--- app/repository_models/curation_concern/work.rb
+++ app/repository_models/curation_concern/work.rb
@@ -15,7 +15,19 @@ module CurationConcern
     def to_solr(solr_doc={}, opts={})
       super(solr_doc, opts)
       Solrizer.set_field(solr_doc, 'generic_type', 'Work', :facetable)
+      Solrizer.set_field(solr_doc, 'admin_unit_hierarchy', get_solr_hierarchy_from_term(self.administrative_unit), :facetable) if self.respond_to?(:administrative_unit)
       return solr_doc
+    end
+
+    def get_solr_hierarchy_from_term(terms, opts={})
+      hierarchies = []
+      terms.each do |term|
+        tree_to_solrize = term.split("::")
+        hierarchies << tree_to_solrize.each_with_index.map do |_, i|
+          tree_to_solrize[0..i].join(':')
+        end
+      end
+      hierarchies.flatten
     end
 
   end
