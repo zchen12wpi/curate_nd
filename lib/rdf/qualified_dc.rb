05507490a4f4dd74c3f320042400e8b1f865d05e
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-04-18 16:07:44 -0400

    Ordering qualified DC terms alphabetically
    Prefer single quotes for non-interpolated strings.

--- lib/rdf/qualified_dc.rb
+++ lib/rdf/qualified_dc.rb
@@ -2,37 +2,37 @@ module RDF
   # This is an approximation of a refinement. At present it is perhaps not
   # adequate, but by marking the property as 'contributor#advisor' the
   # URL resolves to the DC Term 'contributor'
-  class QualifiedDC < Vocabulary("http://purl.org/dc/terms/")
-    property "type".to_sym
-    property "contributor#advisor".to_sym
-    property "creator#author".to_sym
-    property "creator#affiliation".to_sym
-    property "creator#organization".to_sym
-    property "creator#administrative_unit".to_sym
-    property "rights#permissions".to_sym
-    property "contributor#author".to_sym
-    property "publisher#country".to_sym
-    property "identifier#doi".to_sym
-    property "identifier#issn".to_sym
-    property "identifier#other".to_sym
-    property "contributor#repository".to_sym
-    property "contributor#institution".to_sym
-    property "relation#ispartof".to_sym
-    property "title#alternate".to_sym
-    property "date#created".to_sym
-    property "format#mimetype".to_sym
-    property "format#extent".to_sym
-    property "description#technical".to_sym
-    property "description#abstract".to_sym
-    property "description#note".to_sym
-    property "description#methodology".to_sym
-    property "description#data_processing".to_sym
-    property "description#file_structure".to_sym
-    property "description#variable_list".to_sym
-    property "description#code_list".to_sym
-    property "date#digitized".to_sym
-    property "coverage#spatial".to_sym
-    property "coverage#temporal".to_sym
-    property "date#approved".to_sym
+  class QualifiedDC < Vocabulary('http://purl.org/dc/terms/')
+    property 'contributor#advisor'.to_sym
+    property 'contributor#author'.to_sym
+    property 'contributor#institution'.to_sym
+    property 'contributor#repository'.to_sym
+    property 'coverage#spatial'.to_sym
+    property 'coverage#temporal'.to_sym
+    property 'creator#administrative_unit'.to_sym
+    property 'creator#affiliation'.to_sym
+    property 'creator#author'.to_sym
+    property 'creator#organization'.to_sym
+    property 'date#approved'.to_sym
+    property 'date#created'.to_sym
+    property 'date#digitized'.to_sym
+    property 'description#abstract'.to_sym
+    property 'description#code_list'.to_sym
+    property 'description#data_processing'.to_sym
+    property 'description#file_structure'.to_sym
+    property 'description#methodology'.to_sym
+    property 'description#note'.to_sym
+    property 'description#technical'.to_sym
+    property 'description#variable_list'.to_sym
+    property 'format#extent'.to_sym
+    property 'format#mimetype'.to_sym
+    property 'identifier#doi'.to_sym
+    property 'identifier#issn'.to_sym
+    property 'identifier#other'.to_sym
+    property 'publisher#country'.to_sym
+    property 'relation#ispartof'.to_sym
+    property 'rights#permissions'.to_sym
+    property 'title#alternate'.to_sym
+    property 'type'.to_sym
   end
 end
