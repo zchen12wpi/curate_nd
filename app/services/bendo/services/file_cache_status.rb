b5741fdd4a03412a69968bda22ea4a00a5024196
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-01 14:06:37 -0400

    FileCacheStatus tests using a mock API
    Restoring random order to spec helper

--- app/services/bendo/services/file_cache_status.rb
+++ app/services/bendo/services/file_cache_status.rb
@@ -1,6 +1,33 @@
+require 'json'
+
 module Bendo
   module Services
-    class FileCacheStatus
+    module FileCacheStatus
+
+      def call(id: [], handler: BendoApi)
+        ids = Array.wrap(id)
+        results = handler.call(ids)
+        JSON.generate(results)
+      end
+      module_function :call
+
+      module BendoApi
+        module_function
+        def call(ids)
+          raise "Not implmented"
+        end
+      end
+
+      module FakeApi
+        module_function
+
+        def call(ids)
+          results = {}
+          ids.map{ |id| results[id] = true }
+          results
+        end
+      end
+
     end
   end
 end
--- spec/fast_spec_helper.rb
+++ spec/fast_spec_helper.rb
@@ -16,14 +16,8 @@ require 'rspec/rails'
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.run_all_when_everything_filtered = true
   config.infer_spec_type_from_file_location!
-
-  # When issue https://github.com/ndlib/curate_nd/issues/423 is resolved
-  # * Restore config.order = 'random'
-  # * Remove config.seed = '44282'
-  # config.order = 'random'
-  config.seed = '44282'
-
+  config.order = 'random'
+  config.run_all_when_everything_filtered = true
   config.use_transactional_fixtures = false
 end
