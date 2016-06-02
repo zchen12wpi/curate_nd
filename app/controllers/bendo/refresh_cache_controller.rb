ec2839a5f1326073d98a89fb70b226c880872cdc
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-02 13:38:03 -0400

    Stubbing out views for `recall` pages

--- app/controllers/bendo/refresh_cache_controller.rb
+++ app/controllers/bendo/refresh_cache_controller.rb
@@ -4,22 +4,48 @@ module Bendo
 
     # Provide a human-friendly refresh process
     def recall_item
-      respond_to do |format|
-        format.html
+      if item
+        respond_to do |format|
+          format.html { render 'recall_item', status: 302 }
+          format.json { render json: api_response, status: 302 }
+        end
+      else
+        respond_to do |format|
+          format.html { render 'item_not_found', status: 404 }
+          format.json { render json: api_response, status: 404 }
+        end
       end
     end
 
     # JSON API requests only
     def request_item
       respond_to do |format|
-        format.json { render json: make_request(params[:id]) }
+        format.json { render json: api_response }
       end
     end
 
     private
 
-    def make_request(id)
-      JSON.generate({id.to_sym => true})
+    def item
+      begin
+        @item ||= ActiveFedora::Base.find(params[:id], cast: true)
+      rescue ActiveFedora::ObjectNotFoundError
+        @item = nil
+      end
     end
+    helper_method :item
+
+    def api_response
+      @response ||= make_request(params[:id])
+    end
+    helper_method :api_response
+
+    def make_request(id)
+      Bendo::Services::RefreshFileCache.call(
+        id: id,
+        handler: Bendo::Services::FakeApi
+      )
+    end
+
   end
 end
