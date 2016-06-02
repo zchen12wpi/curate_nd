c307b1ac7a08e32349ff61940552da3bbef30ea8
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-02 15:21:37 -0400

    Adding layout to human-facing pages

--- app/controllers/bendo/refresh_cache_controller.rb
+++ app/controllers/bendo/refresh_cache_controller.rb
@@ -3,18 +3,25 @@ module Bendo
   class RefreshCacheController < BendoController
     include Sufia::Noid
     before_filter :normalize_identifier
+    layout 'common_objects'
 
     # Provide a human-friendly refresh process
     def recall_item
       if item
         respond_to do |format|
           format.html { render 'recall_item', status: 302 }
-          format.json { render json: api_response.body, status: api_response.status }
+          format.json { render(
+            json: api_response.body,
+            status: api_response.status
+          )}
         end
       else
         respond_to do |format|
           format.html { render 'item_not_found', status: 404 }
-          format.json { render json: {}, status: 404 }
+          format.json { render(
+            json: {},
+            status: 404
+          )}
         end
       end
     end
@@ -22,7 +29,10 @@ module Bendo
     # JSON API requests only
     def request_item
       respond_to do |format|
-        format.json { render json: api_response.body, status: api_response.status }
+        format.json { render(
+          json: api_response.body,
+          status: api_response.status
+        )}
       end
     end
 
@@ -36,6 +46,11 @@ module Bendo
       end
     end
     helper_method :item
+
+    def pid
+      params[:id]
+    end
+    helper_method :pid
 
     def api_response
       @response ||= make_request(params[:id])
