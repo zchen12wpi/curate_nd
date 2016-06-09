2e6abe795f660f1f633e15600da1da9376cd74d7
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-09 13:30:22 -0400

    Correcting parameter name in specs
    Including stub describe blocks for DatastreamPresenter tests.

---
+++ spec/services/bendo/datastream_presenter_spec.rb
@@ -0,0 +1,15 @@
+require 'fast_spec_helper'
+require 'bendo/datastream_presenter'
+
+module Bendo
+  RSpec.describe DatastreamPresenter do
+    describe '#valid?' do
+    end
+
+    describe '#location' do
+    end
+
+    describe 'item_path' do
+    end
+  end
+end
--- spec/services/bendo/services/file_cache_status_spec.rb
+++ spec/services/bendo/services/file_cache_status_spec.rb
@@ -5,21 +5,21 @@ module Bendo
   module Services
     RSpec.describe FileCacheStatus do
       describe '.call' do
-        context 'with no IDs' do
+        context 'with no identifiers' do
           subject { described_class.call(handler: FakeApi) }
           its(:body) { is_expected.to eq({}) }
           its(:status) { is_expected.to eq(200) }
         end
 
-        context 'with a single ID' do
-          subject { described_class.call(id: '1001001001', handler: FakeApi) }
+        context 'with a single identifier' do
+          subject { described_class.call(item_slugs: '1001001001', handler: FakeApi) }
           its(:body) { is_expected.to be_kind_of(Hash) }
           its(:body) { is_expected.to include('1001001001') }
           its(:status) { is_expected.to eq(200) }
         end
 
-        context 'with an Array of IDs' do
-          subject { described_class.call(id: ['1001001001', 'Masks'], handler: FakeApi) }
+        context 'with an Array of identifiers' do
+          subject { described_class.call(item_slugs: ['1001001001', 'Masks'], handler: FakeApi) }
           its(:body) { is_expected.to be_kind_of(Hash) }
           its(:body) { is_expected.to include('1001001001') }
           its(:body) { is_expected.to include('Masks') }
