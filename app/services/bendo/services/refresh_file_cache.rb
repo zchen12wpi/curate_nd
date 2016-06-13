ae302f400be7aafe045dc70c786c8ed21ec4a23a
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-13 14:05:02 -0400

    Use a prematurely terminated GET request to warm the Bendo cache
    As per the API spec. This implementation is kind of hairy. Because the
    connection is closed while the response body is still being processed
    the request ends up nil. This causes an NoMethodError because read_body
    tries to check if the request is `#closed?`.

--- app/services/bendo/services/refresh_file_cache.rb
+++ app/services/bendo/services/refresh_file_cache.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 module Bendo
   module Services
     module RefreshFileCache
@@ -17,13 +19,35 @@ module Bendo
           # of waiting for all of the body to be sent. The easiest way to do
           # this is with HTTP streaming.
           body = slugs.each_with_object({}) do |item_slug, memo|
-            memo[item_slug] = true
+            uri = URI.parse(item_url(item_slug))
+            status = 500
+            begin
+              Net::HTTP.start(uri.host, uri.port) do |http|
+                request = Net::HTTP::Get.new uri
+
+                http.request request do |response|
+                  response.read_body do |chunk|
+                    status = response.code.to_i
+                    http.finish
+                  end
+                end
+              end
+            rescue NoMethodError
+              logger.info "Prematurely terminated request for #{uri} to warm the cache"
+            end
+            memo[item_slug] = status
           end
+
           Response.new(200, body.to_json)
         end
         module_function :call
-      end
 
+        def item_url(item_slug)
+          Bendo.item_url(item_slug)
+        end
+        module_function :item_url
+
+      end
     end
   end
 end
