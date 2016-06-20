c1ab7c6641d9a02f6bd820438578d65bdc1d1d6b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-06-20 09:23:12 -0400

    Consolidating initializers of like name
    Instead of all of the disparate initializers, attempting to gather them
    into a common locations.

--- config/initializers/curate_config.rb
+++ config/initializers/curate_config.rb
@@ -23,3 +23,6 @@ Curate.configure do |config|
     end
   end
 end
+
+# Add :has_profile predicate for use in ActiveModel (RELS-EXT) relationships
+ActiveFedora::Predicates.set_predicates("http://projecthydra.org/ns/relations#"=>{:has_profile => "hasProfile"})
--- config/initializers/curate_initializer.rb
+++
@@ -1,2 +0,0 @@
-# Add :has_profile predicate for use in ActiveModel (RELS-EXT) relationships
-ActiveFedora::Predicates.set_predicates("http://projecthydra.org/ns/relations#"=>{:has_profile => "hasProfile"})
