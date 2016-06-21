e6849107d12b659412fba83c5e7f42b17a6446ae
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2016-06-21 09:17:30 -0400

    Adding rudimentary relationship indexing
    While this is incomplete, it is aspects of the indexing that can occur.

--- app/repository_models/curation_concern/is_member_of_library_collection.rb
+++ app/repository_models/curation_concern/is_member_of_library_collection.rb
@@ -5,5 +5,15 @@ module CurationConcern
     included do
       has_and_belongs_to_many :library_collections, property: :is_member_of_collection, class_name: "ActiveFedora::Base"
     end
+
+    # These are the easy library collection relationships that can be solrized without the full anctics of crawling the relationship graph.
+    #
+    # @see Curate::LibraryCollectionIndexingAdapter.write_document_attributes_to_index_layer
+    def to_solr(solr_doc={}, opts={})
+      super(solr_doc, opts)
+      solr_doc[Curate::LibraryCollectionIndexingAdapter::SOLR_KEY_PARENT_PIDS_FACETABLE] = self.library_collection_ids
+      solr_doc[Curate::LibraryCollectionIndexingAdapter::SOLR_KEY_PARENT_PIDS] = self.library_collection_ids
+      solr_doc
+    end
   end
 end
