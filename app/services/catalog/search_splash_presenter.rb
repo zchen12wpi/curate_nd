ba9ae13122df2ca0a6fa0700656412f9a420ef02
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-23 14:53:08 -0400

    Tests passing for existing splash use cases

--- app/services/catalog/search_splash_presenter.rb
+++ app/services/catalog/search_splash_presenter.rb
@@ -1,34 +1,54 @@
 module Catalog
   module SearchSplashPresenter
-    module_function
+    ARTICLE_SPLASH = 'Articles & Publications'.freeze
+    DATASET_SPLASH = 'Datasets & Related Materials'.freeze
+        ETD_SPLASH = 'Theses & Dissertations'.freeze
 
     def call(params)
-      category_key = :human_readable_type_sim
-
-      if params.include?(:f) && params[:f][category_key]
-        categories = params[:f][category_key]
-
-        if categories.present? && categories.count == 1
-          category = categories.first
-        else
-          category = nil
+      if category_present?(params)
+        if category_match?(params, ['Article'])
+          ARTICLE_SPLASH
+        elsif category_match?(params, ['Dataset'])
+          DATASET_SPLASH
         end
-      else
-        category = nil
-      end
-
-      splash = case category
-      when 'Article'
-        'Articles & Publications'
-      when 'Dataset'
-        'Datasets & Related Materials'
-      when 'Thesis or Dissertation', "Master's Thesis", 'Doctoral Dissertation'
-        'Theses & Dissertations'
+      elsif inclusive_category_present?(params)
+        if inclusive_category_match?(
+          params,
+          [
+            'Doctoral Dissertation',
+            "Master's Thesis"
+          ])
+          ETD_SPLASH
+        end
       else
         nil
       end
-
-      splash
     end
+    module_function :call
+
+    private
+
+    module_function
+
+    def category_key
+      :human_readable_type_sim
+    end
+
+    def category_present?(params)
+      params.key?(:f) && params[:f].fetch(category_key, nil)
+    end
+
+    def category_match?(params, value)
+      params[:f][category_key] == value
+    end
+
+    def inclusive_category_present?(params)
+      params.key?(:f_inclusive) && params[:f_inclusive].fetch(category_key, nil)
+    end
+
+    def inclusive_category_match?(params, value)
+      params[:f_inclusive][category_key] == value
+    end
+
   end
 end
--- spec/services/catalog/search_splash_presenter_spec.rb
+++ spec/services/catalog/search_splash_presenter_spec.rb
@@ -4,16 +4,22 @@ require 'catalog/search_splash_presenter'
 module Catalog
   RSpec.describe SearchSplashPresenter do
     describe '.call' do
+      context 'empty params' do
+        let(:empty_params) { Hash.new }
+        subject { described_class.call(empty_params) }
+        it { is_expected.to eq(nil) }
+      end
+
       context 'Articles' do
         let(:article_params) { { f: { human_readable_type_sim: ['Article'] } } }
         subject { described_class.call(article_params) }
-        it { is_expected.to eq('Articles & Publications') }
+        it { is_expected.to eq(described_class::ARTICLE_SPLASH) }
       end
 
       context 'Datasets' do
         let(:dataset_params) { { f: { human_readable_type_sim: ['Dataset'] } } }
         subject { described_class.call(dataset_params) }
-        it { is_expected.to eq('Datasets & Related Materials') }
+        it { is_expected.to eq(described_class::DATASET_SPLASH) }
       end
 
       context 'Theses and Dissertations' do
@@ -28,7 +34,7 @@ module Catalog
           }
         end
         subject { described_class.call(etd_params) }
-        it { is_expected.to eq('Theses & Dissertations') }
+        it { is_expected.to eq(described_class::ETD_SPLASH) }
       end
     end
   end
