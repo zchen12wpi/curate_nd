06bb6daedc303c597b0e58b8b8cf32982b97d018
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-06-23 11:12:16 -0400

    Testing existing use cases
    Yes, one of them is failing

--- spec/services/catalog/search_splash_presenter_spec.rb
+++ spec/services/catalog/search_splash_presenter_spec.rb
@@ -3,5 +3,33 @@ require 'catalog/search_splash_presenter'
 
 module Catalog
   RSpec.describe SearchSplashPresenter do
+    describe '.call' do
+      context 'Articles' do
+        let(:article_params) { { f: { human_readable_type_sim: ['Article'] } } }
+        subject { described_class.call(article_params) }
+        it { is_expected.to eq('Articles & Publications') }
+      end
+
+      context 'Datasets' do
+        let(:dataset_params) { { f: { human_readable_type_sim: ['Dataset'] } } }
+        subject { described_class.call(dataset_params) }
+        it { is_expected.to eq('Datasets & Related Materials') }
+      end
+
+      context 'Theses and Dissertations' do
+        let(:etd_params) do
+          {
+            f_inclusive: {
+              human_readable_type_sim: [
+                'Doctoral Dissertation',
+                "Master's Thesis"
+              ]
+            }
+          }
+        end
+        subject { described_class.call(etd_params) }
+        it { is_expected.to eq('Theses & Dissertations') }
+      end
+    end
   end
 end
