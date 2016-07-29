44ce3f8e51c324b6913c2023049a6d045dcaf878
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-07-29 09:54:48 -0400

    Getting CollectionDecorator to “shameless green”

--- app/services/catalog/collection_decorator.rb
+++ app/services/catalog/collection_decorator.rb
@@ -1,7 +1,28 @@
 module Catalog
   module CollectionDecorator
-    def self.call(identifier, data_source: ActiveFedoraAdapter)
+    def self.call(scope, data_source: ActiveFedoraAdapter)
+      identifier = pid_from_scope(scope)
       data_source.call(identifier)
+    end
+
+    def self.pid_from_scope(scope, delimiter: '/')
+      return '' if scope.nil? || scope.empty?
+      slug = scope.split(delimiter).last
+      extract_id(slug)
+    end
+
+    def self.title_from_scope(scope, delimiter: '/')
+      return '' if scope.nil? || scope.empty?
+      slug = scope.split(delimiter).last
+      extract_title(slug)
+    end
+
+    def self.extract_id(slug, delimiter: '|')
+      slug.split(delimiter).last.to_s
+    end
+
+    def self.extract_title(slug, delimiter: '|')
+      slug.split(delimiter).first.to_s
     end
 
     module FakeAdapter
--- spec/services/catalog/collection_decorator_spec.rb
+++ spec/services/catalog/collection_decorator_spec.rb
@@ -4,10 +4,60 @@ require 'catalog/collection_decorator'
 module Catalog
   RSpec.describe CollectionDecorator do
     describe '.call' do
-      context 'empty attribute' do
+      context 'with nil' do
+        let(:empty_attribute) { nil }
+        subject { described_class.call(empty_attribute, data_source: described_class::FakeAdapter) }
+        it { is_expected.to eq('') }
+      end
+
+      context 'with empty string' do
         let(:empty_attribute) { '' }
         subject { described_class.call(empty_attribute, data_source: described_class::FakeAdapter) }
         it { is_expected.to eq('') }
+      end
+
+      context 'with only pid' do
+        let(:single_pid) { 'und:1234' }
+        subject { described_class.call(single_pid, data_source: described_class::FakeAdapter) }
+        it { is_expected.to eq('und:1234') }
+      end
+
+      context 'with tile and embedded pid' do
+        let(:single_slug) { 'Helpful Title|und:1337' }
+        subject { described_class.call(single_slug, data_source: described_class::FakeAdapter) }
+        it { is_expected.to eq('und:1337') }
+      end
+
+      context 'with pid path' do
+        let(:pid_path) { 'und:1234/und:4567/und:1337' }
+        subject { described_class.call(pid_path, data_source: described_class::FakeAdapter) }
+        it { is_expected.to eq('und:1337') }
+      end
+
+      context 'with title and embedded pid path' do
+        let(:slug_path) { 'Large Collection|und:1234/Arbitrary Container|und:4567/Helpful Title|und:1337' }
+        subject { described_class.call(slug_path, data_source: described_class::FakeAdapter) }
+        it { is_expected.to eq('und:1337') }
+      end
+    end
+
+    describe '.title_from_scope' do
+      context 'with tile and embedded pid' do
+        let(:single_slug) { 'Helpful Title|und:1337' }
+        subject { described_class.title_from_scope(single_slug) }
+        it { is_expected.to eq('Helpful Title') }
+      end
+
+      context 'with pid path' do
+        let(:pid_path) { 'und:1234/und:4567/und:1337' }
+        subject { described_class.title_from_scope(pid_path) }
+        it { is_expected.to eq('und:1337') }
+      end
+
+      context 'with title and embedded pid path' do
+        let(:slug_path) { 'Large Collection|und:1234/Arbitrary Container|und:4567/Helpful Title|und:1337' }
+        subject { described_class.title_from_scope(slug_path) }
+        it { is_expected.to eq('Helpful Title') }
       end
     end
   end
