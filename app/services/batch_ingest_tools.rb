588f1d0742a5802aee8cb9c867193f35e04c540a
msuhovec <suhovecky@nd.edu>
2016-08-10 15:49:17 -0400

    change BatchIngestTools to queue start-reingest task
    #491

--- app/services/batch_ingest_tools.rb
+++ app/services/batch_ingest_tools.rb
@@ -11,13 +11,23 @@ class BatchIngestTools
 
     make_job_id
     create_batch_job
-    add_job_file('fedora-pids', pid_list)
+    add_job_file('fedora-pids', pid_list, false)
+    task_list = make_task_list
+    add_job_file('JOB', task_list, true)
     submit_batch_job
 
   end
 
   def self.make_job_id
     @job_id = Time.now.strftime 'reingest%Y%b%e%H%M%s'
+  end
+
+  # create Contents of JOB file to start reingest in batch ingest system
+  def self.make_task_list
+    list = {}
+    list['Todo'] = []
+    list['Todo'].push( 'start-reingest' )
+    return list
   end
 
   # does PUT /jobs/:jobid to create data dir on batch ingest side
@@ -30,12 +40,16 @@ class BatchIngestTools
   end
 
   # does PUT /jobs/:jobid to create data dir on batch ingest side
-  def self.add_job_file(name, content)
+  def self.add_job_file(name, content, want_json)
     url = 'http://localhost:15000/jobs/' + @job_id + "/files/" + name
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Put.new(uri.path)
-    request.body = content.to_s
+    if want_json == true 
+      request.body = JSON.generate(content)
+    else
+      request.body = content.to_s
+    end
     http.request(request)
   end
 
