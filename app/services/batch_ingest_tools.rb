d0396104e0303dd41b5d07fc3b7058b3fb4da422
msuhovec <suhovecky@nd.edu>
2016-08-10 15:51:09 -0400

    rubocop changes :)

--- app/services/batch_ingest_tools.rb
+++ app/services/batch_ingest_tools.rb
@@ -6,16 +6,13 @@ require 'uri'
 # Use Batch Ingest HTTP Interface to submit modified fedora objects
 # that contain bendo items to batch ingest system, for possible reingest
 class BatchIngestTools
-
   def self.submit_pidlist(pid_list)
-
     make_job_id
     create_batch_job
     add_job_file('fedora-pids', pid_list, false)
     task_list = make_task_list
     add_job_file('JOB', task_list, true)
     submit_batch_job
-
   end
 
   def self.make_job_id
@@ -26,8 +23,8 @@ class BatchIngestTools
   def self.make_task_list
     list = {}
     list['Todo'] = []
-    list['Todo'].push( 'start-reingest' )
-    return list
+    list['Todo'].push('start-reingest')
+    list
   end
 
   # does PUT /jobs/:jobid to create data dir on batch ingest side
@@ -41,26 +38,26 @@ class BatchIngestTools
 
   # does PUT /jobs/:jobid to create data dir on batch ingest side
   def self.add_job_file(name, content, want_json)
-    url = 'http://localhost:15000/jobs/' + @job_id + "/files/" + name
+    url = 'http://localhost:15000/jobs/' + @job_id + '/files/' + name
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Put.new(uri.path)
-    if want_json == true 
-      request.body = JSON.generate(content)
-    else
-      request.body = content.to_s
-    end
+    request.body = if want_json == true
+                     JSON.generate(content)
+                   else
+                     content.to_s
+                   end
     http.request(request)
   end
 
   # does POST /jobs/:jobid/queue to start process on batch ingest side
   def self.submit_batch_job
-    url = 'http://localhost:15000/jobs/' + @job_id + "/queue"
+    url = 'http://localhost:15000/jobs/' + @job_id + '/queue'
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
 
-    http.request_post(uri.path, 'submit') { |response|
-	p response.code
-    }
+    http.request_post(uri.path, 'submit') do |response|
+      p response.code
+    end
   end
 end
