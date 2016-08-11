d4978adbe301bc5d0ecb9c3bd6a35f03d0983e74
msuhovec <suhovecky@nd.edu>
2016-08-11 18:52:03 -0400

    added timestamps file and range query
    #491

--- app/services/solr_tools.rb
+++ app/services/solr_tools.rb
@@ -1,43 +1,46 @@
 require 'rsolr'
+require 'time'
 
+# Tools to perform SOLR query via rsolr gem
 class SolrTools
   # Check SOLR for records since timestamp file
   def self.changed_since(timestamp_file)
     from = get_fromtime(timestamp_file)
     to = get_totime
-    pid_list = get_solr_list(from, to)
+    get_solr_list(from, to)
   end
 
-
+  # fromtime is mod time of timestamp filr- * if it doesn't exist
+  #  SOLR queries accept time ranges in iso8601 format
   def self.get_fromtime(timestamp_file)
-    return 'NOW-1DAY' 
+    return '*' unless FileTest.exists?(timestamp_file)
+    File.mtime(timestamp_file).utc.iso8601
   end
 
   def self.get_totime
-    return 'NOW'
+    'NOW'
   end
 
+  # query solr over the specificied time range
   def self.get_solr_list(fromtime, totime)
     docs = []
     start = 0
     loop do
       solr_params = {
-          rows: 100,
-          start: start,
-          fq: "id: und* AND timestamp:[ * TO NOW]",
-          fl: "id",
+        rows: 100,
+        start: start,
+        fq: "id: und* AND timestamp:[ #{fromtime} TO #{totime}]",
+        fl: 'id'
       }
       start += 100
 
       new_docs = rsolr.select params: solr_params
 
-      break if new_docs["response"]["docs"].count == 0
-      docs += new_docs["response"]["docs"]
+      break if new_docs['response']['docs'].count == 0
+      docs += new_docs['response']['docs']
     end
 
-    if docs.count > 0
-      return  docs 
-    end
+    return docs if docs.count > 0
   end
 
   def self.rsolr
@@ -51,7 +54,7 @@ class SolrTools
     @solr_url[Rails.env]['url']
   end
 
-  #build query 
-  def self.query_params(fromtime, totime)
+  # build query
+  def self.query_params(_fromtime, _totime)
   end
 end
