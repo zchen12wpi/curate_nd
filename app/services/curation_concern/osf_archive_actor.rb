28edc1f2aad821c589fdaedad0145c96ae660ee7
Justin Gondron <jgondron@nd.edu>
2016-09-07 16:48:24 -0400

    DLTP-538: Remove create
    Added an exception to OsfArchiveActor#create to prevent usage, since creation of OSF archives will happen via batch imports.

--- app/services/curation_concern/osf_archive_actor.rb
+++ app/services/curation_concern/osf_archive_actor.rb
@@ -1,4 +1,7 @@
 module CurationConcern
   class OsfArchiveActor < GenericWorkActor
+    def create
+      raise NotImplementedError, "This is not implemented as we don't create an OSF Archive via the Actor"
+    end
   end
 end
