a7f797f7287cb9059e06c2a6525ef1d086d68ebb
LaRita Robinson <lrobins5@nd.edu>
2016-09-09 16:18:25 -0400

    Add specs for BatchIngestor

--- app/services/batch_ingestor.rb
+++ app/services/batch_ingestor.rb
@@ -1,7 +1,6 @@
 require 'json'
 require 'net/http'
 require 'time'
-require 'uri'
 
 class BatchIngestor
 
@@ -20,24 +19,22 @@ class BatchIngestor
     job_id_prefix = 'reingest'
     task_function_name = 'start-reingest'
     content_file_name = 'fedora_pids'
-    new(job_id_prefix, task_function_name, content_file_name, content_data).submit
+    new(job_id_prefix, task_function_name, content_file_name, content_data).submit_ingest
   end
 
   def self.start_osf_archive_ingest(content_data)
     job_id_prefix = 'osfarchive'
-    todo_function_name = 'start-osf-archive-ingest'
+    task_function_name = 'start-osf-archive-ingest'
     content_file_name = 'osf_projects'
     content_data[:project_url] = get_osf_url(content_data[:project_identifier])
     new(job_id_prefix, task_function_name, content_file_name, content_data).submit_ingest
   end
 
-  private
-
   def submit_ingest
     @job_id = make_job_id
     create_batch_job
-    add_job_file
-    add_job_file('JOB', task_list, true)
+    add_job_file(content_file_name, content_data)
+    add_job_file('JOB', task_list)
     submit_batch_job
   end
 
@@ -45,8 +42,10 @@ class BatchIngestor
     'https://osf.io' + '/' + project_identifier + '/'
   end
 
+  private
+
   def make_job_id
-    time_format = job_id_prefix + '%Y%b%e%H%M%s'
+    time_format = job_id_prefix + '%Y%b%d%H%M%s'
     return Time.now.strftime(time_format)
   end
 
@@ -64,32 +63,40 @@ class BatchIngestor
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Put.new(uri.path)
-    http.request(request)
+    response = http.request(request)
+    return true if response.code == '200'
+    report_to_airbrake('create_batch_job', response.code)
   end
 
   # does PUT /jobs/:jobid to create data dir on batch ingest side
-  def add_job_file
+  def add_job_file(name, data)
     want_json = true
-    url = SERVER_URL + job_id + '/files/' + content_file_name
+    url = SERVER_URL + job_id + '/files/' + name
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Put.new(uri.path)
-    request.body = if want_json == true
-                     JSON.generate(content_data)
-                   else
-                     content_data.to_s
-                   end
-    http.request(request)
+    request.body = JSON.generate(data)
+    response = http.request(request)
+    return true if response.code == '200'
+    report_to_airbrake('add_job_file', response.code)
   end
 
   # does POST /jobs/:jobid/queue to start process on batch ingest side
   def submit_batch_job
-    url = SERVER_URL + @job_id + '/queue'
+    url = SERVER_URL + job_id + '/queue'
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
+    response = http.request_post(uri.path, 'submit')
+    return true if response.code == '200'
+    report_to_airbrake('submit_batch_job', response.code)
+  end
 
-    http.request_post(uri.path, 'submit') do |response|
-      p response.code
-    end
+  class SubmitError < RuntimeError
+  end
+
+  def report_to_airbrake(method_name, code)
+    exception = SubmitError.new("HTTP request failed with status " + code)
+    Airbrake.notify_or_ignore(error_class: exception.class, error_message: exception, parameters: { task_name: task_function_name, method_name: method_name ,job_input: content_data })
+    raise exception
   end
 end
--- spec/services/batch_ingestor_spec.rb
+++ spec/services/batch_ingestor_spec.rb
@@ -1,5 +1,86 @@
 require 'spec_helper'
 
-describe BatchIngestor do
-  
+RSpec::Matchers.define :a_put do |expected_path|
+  match { |actual| actual.path == expected_path }
+end
+
+RSpec::Matchers.define :a_put_with do |expected_body|
+  match { |actual| actual.body == expected_body }
+end
+
+RSpec.describe BatchIngestor do
+
+  describe '#submit_ingest' do
+    let(:subject) { described_class.new('job_id', 'task', 'file_name', ['abcde', 'defgh', 'ijklm']).submit_ingest }
+    let(:response) { instance_double(Net::HTTPResponse, code: '200') }
+    let(:http) { instance_double(Net::HTTP, request_post: response) }
+
+    before(:each) do
+      allow(Time).to receive(:now).and_return(Time.new('2011/01/01'))
+    end
+
+    it 'submits 4 correctly ordered requests' do
+      allow(Net::HTTP).to receive(:new).and_return(http)
+      expect(http).to receive(:request)
+        .with(a_put('/jobs/job_id2011Jan0100001293858000'))
+        .and_return(response)
+        .ordered
+      expect(http).to receive(:request)
+        .with(a_put('/jobs/job_id2011Jan0100001293858000/files/file_name'))
+        .and_return(response)
+        .ordered
+      expect(http).to receive(:request)
+        .with(a_put('/jobs/job_id2011Jan0100001293858000/files/JOB'))
+        .and_return(response)
+        .ordered
+      expect(http).to receive(:request_post)
+        .with('/jobs/job_id2011Jan0100001293858000/queue','submit')
+        .and_return(response)
+        .ordered
+      subject
+    end
+
+    it 'will report to Airbrake if response code not 200' do
+      stub_request(:any, 'http://localhost:15000/jobs/job_id2011Jan0100001293858000')
+        .to_return(status: 500)
+      expect(Airbrake).to receive(:notify_or_ignore)
+      expect { subject }.to raise_error("HTTP request failed with status 500")
+    end
+
+    it 'submits the content data' do
+      allow(Net::HTTP).to receive(:new)
+        .and_return(http)
+      allow(http).to receive(:request)
+        .and_return(response)
+        .at_least(:once)
+      expect(http).to receive(:request)
+        .with(a_put_with('["abcde","defgh","ijklm"]'))
+        .and_return(response)
+        .once
+      subject
+    end
+
+    it 'submits the task list' do
+      allow(Net::HTTP).to receive(:new)
+        .and_return(http)
+      allow(http).to receive(:request)
+        .and_return(response)
+        .at_least(:once)
+      expect(http).to receive(:request)
+        .with(a_put_with('{"Todo":["task"]}'))
+        .and_return(response)
+        .once
+      subject
+    end
+  end
+
+  describe '#get_osf_url' do
+    xit 'determines the project url' do
+    end
+  end
+
+  describe '.start_osf_archive_ingest' do
+    xit 'appends the content data with a project url' do
+    end
+  end
 end
