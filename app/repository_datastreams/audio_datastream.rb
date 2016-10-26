fa160689f3bac152c369a133d1a59c017fee54e2
LaRita Robinson <lrobins5@nd.edu>
2016-10-26 10:25:09 -0400

    Start setup for views structure
    Copy partials from another works type for basic show/edit/new structure.
    Complete repository datastream & model.

---
+++ app/repository_datastreams/audio_datastream.rb
@@ -0,0 +1,117 @@
+require File.expand_path('../../../lib/rdf/qualified_dc', __FILE__)
+require File.expand_path('../../../lib/rdf/ebucore', __FILE__)
+require File.expand_path('../../../lib/rdf/relators', __FILE__)
+require File.expand_path('../../../lib/rdf/nd', __FILE__)
+class AudioDatastream < ActiveFedora::NtriplesRDFDatastream
+  map_predicates do |map|
+
+    map.title(in: RDF::DC) do |index|
+      index.as :stored_searchable
+    end
+
+    map.description(:in => RDF::DC) do |index|
+      index.type :text
+      index.as :stored_searchable
+    end
+
+    map.alternate_title(to: 'alternative', in: RDF::DC) do |index|
+      index.as :stored_searchable
+    end
+
+    #performer(s)
+    map.performer(to: 'prf', in RDF::Relators) do |index|
+      index.as :stored_searchable
+    end
+
+    #composer(s)
+    map.composer(to: 'cmp', in RDF::Relators) do |index|
+      index.as :stored_searchable
+    end
+
+    #conductor(s)
+    map.conductor(to: 'cnd', in RDF::Relators) do |index|
+      index.as :stored_searchable
+    end
+
+    #author(s) of material used in the work
+    map.work_author(to: 'aut', in RDF::Relators) do |index|
+      index.as :stored_searchable
+    end
+
+    #Interviewer(s)
+    map.interviewer(to: 'ivr', in RDF::Relators) do |index|
+      index.as :stored_searchable
+    end
+
+    #Interviewee(s)
+    map.interviewee(to: 'ive', in RDF::Relators) do |index|
+      index.as :stored_searchable
+    end
+
+    #Producer(s)
+    map.producer(to: 'pro', in RDF::Relators) do |index|
+      index.as :stored_searchable
+    end
+
+    map.creator(to: 'creator', in: RDF::DC) do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    map.contributor(to: 'contributor', in: RDF::DC) do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    map.duration(to: 'duration', in RDF::Ebucore)
+
+    map.subject(to: 'subject', in: RDF::DC) do |index|
+      index.type :text
+      index.as :stored_searchable
+    end
+
+    map.genre(to: 'hasGenre', in: RDF::Ebucore) do |index|
+      index.as :stored_searchable
+    end
+
+    map.is_part_of({to: 'isPartOf', in: RDF::DC}) do |index|
+      index.as :stored_searchable
+    end
+
+    map.original_media_source(to: 'isDerivedFrom', in: RDF::Ebucore)
+
+    #date recorded
+    map.date_created(to: 'created', :in => RDF::DC) do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    map.publisher({in: RDF::DC}) do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    map.publication_date({to: 'issued', in: RDF::DC})
+
+    map.administrative_unit(to: 'creator#administrative_unit', in: RDF::QualifiedDC) do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    map.source(to: 'source', in: RDF::DC) do |index|
+      index.type :text
+      index.as :stored_searchable
+    end
+
+    map.relation(:in => RDF::DC) do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    map.alephIdentifier(:in =>RDF::ND) do |index|
+      index.as :stored_searchable
+    end
+
+    map.affiliation(to: 'creator#affiliation', in: RDF::QualifiedDC) do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+    map.rights(:in => RDF::DC) do |index|
+      index.as :stored_searchable, :facetable
+    end
+  end
+end
