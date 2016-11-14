2486fa655f829577941e86a3ee3f685a2b9bb789
LaRita Robinson <lrobins5@nd.edu>
2016-11-14 16:42:14 -0500

    Correct with_json_mapper shared spec

--- spec/support/shared/with_json_mapper.rb
+++ spec/support/shared/with_json_mapper.rb
@@ -1,8 +1,6 @@
 shared_examples 'with_json_mapper' do
   context 'behavior' do
     subject { described_class.new }
-    it 'responds to method as_jsonld' do
       it { is_expected.to respond_to(:as_jsonld) }
-    end
   end
 end
