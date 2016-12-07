535333924ae5fabb5fd51ed9e70180a62cad2bca
Banurekha <banurekha.l@gmail.com>
2016-12-07 11:21:36 -0500

    Always reindex collection as part of active_fedora update_index
    Solr documents are updated on each save and collection object lose some of the collection related solr fields during update. So we are tapping into update_index to reindex collection so that they are always available.

--- spec/support/shared/shared_examples_is_a_curation_concern_actor.rb
+++ spec/support/shared/shared_examples_is_a_curation_concern_actor.rb
@@ -35,7 +35,6 @@ shared_examples 'is_a_curation_concern_actor' do |curation_concern_class, curati
       }
 
       it 'should be successful in updating attributes' do
-        expect(Curate.relationship_reindexer).to receive(:call).and_return(true)
         expect(subject.create).to eq(true)
 
         expect(curation_concern).to be_persisted
