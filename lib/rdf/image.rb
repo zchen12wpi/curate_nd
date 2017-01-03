a71fdc09c7c91a9f917d16885dc411f387386e0b
Ruth Tillman <rtillman@nd.edu>
2017-01-03 15:57:38 -0500

    Added VRACore fields
    And added removed fake properties we'd created to avoid confusion. None were in use.

--- lib/rdf/image.rb
+++ lib/rdf/image.rb
@@ -1,15 +1,17 @@
+# This is deprecated for actual VRA Core availability. None of these fields were in use so deprecating is ok. Not only was there a manual check, there was some unpleasant grepping.
+# source in Image work was mapped to existing dc:source
 module RDF
   class Image < Vocabulary("http://nd.edu/image#")
-    property :category
-    property :location
-    property :measurements
-    property :material
-    property :source
-    property :inscription
-    property :StateEdition
-    property :TEXTREF
-    property :cultural_context
-    property :style_period
-    property :technique
+#    property :category
+#    property :location
+#    property :measurements
+#    property :material
+#    property :source
+#    property :inscription
+#    property :StateEdition
+#    property :TEXTREF
+#    property :cultural_context
+#    property :style_period
+#    property :technique
   end
 end
