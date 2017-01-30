5fd0f7b35ddeb88002f53502c855f070e4ecda69
Ruth Tillman <rtillman@nd.edu>
2017-01-30 12:00:32 -0500

    Added Controller for Video

---
+++ app/controllers/curation_concern/videos_controller.rb
@@ -0,0 +1,16 @@
+class CurationConcern::VideosController < CurationConcern::GenericWorksController
+  self.curation_concern_type = Video
+
+  def setup_form
+    if action_name == 'new'
+      curation_concern.creator << current_user.name if curation_concern.creator.empty? && !current_user.can_make_deposits_for.any?
+      curation_concern.record_editors << current_user.person unless curation_concern.record_editors.present?
+    end
+    curation_concern.record_editors.build
+    curation_concern.record_editor_groups.build
+    curation_concern.record_viewers.build
+    curation_concern.record_viewer_groups.build
+  end
+  protected :setup_form
+
+end
