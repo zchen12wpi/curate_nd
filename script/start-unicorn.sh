043cc4c0f0eb4bf016f065174b3a2079467ed2c8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-02-22 16:14:04 -0500

    Removing writing logs to unicorn stdout log
    The following line shows why we are likely writing $stderr to unicorn
    https://bogomips.org/unicorn.git/tree/lib/unicorn.rb?h=v4.6.3#n76
    
    As I was debugging this I encountered a caching-type behavior. If I
    changed the code on how unicorn starts then deployed the changes, the
    restart unicorn wouldn't always use the right RAILS_ENV.
    
    What I needed to do to get this working was to SSH onto the machine and
    stop the unicorn process then restart it. It was as thought the -E
    option were either cached or continued to be used.

--- script/start-unicorn.sh
+++ script/start-unicorn.sh
@@ -7,4 +7,6 @@ export PATH=/opt/ruby/current/bin:$PATH
 
 source /home/app/curatend/current/script/get-env.sh
 cd $RAILS_ROOT
-bundle exec unicorn -D -E deployment -c $RAILS_ROOT/config/unicorn.rb
+# This looks to be a bit of a misnomer; The script that is being run is wrongly
+# interpretting
+bundle exec unicorn -D -E $RAILS_ENV -c $RAILS_ROOT/config/unicorn.rb
