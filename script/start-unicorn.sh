8b2c25cac67a270a758c54a13a56d129db54ec27
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2017-02-23 09:00:54 -0500

    Closing issue DLTP-792
    $ ./script/close-issue DLTP-792

--- script/start-unicorn.sh
+++ script/start-unicorn.sh
@@ -7,6 +7,4 @@ export PATH=/opt/ruby/current/bin:$PATH
 
 source /home/app/curatend/current/script/get-env.sh
 cd $RAILS_ROOT
-# This looks to be a bit of a misnomer; The script that is being run is wrongly
-# interpretting
 bundle exec unicorn -D -E $RAILS_ENV -c $RAILS_ROOT/config/unicorn.rb
