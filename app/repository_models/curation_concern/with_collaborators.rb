8d795d9fb78047dcc7d29641d29f66055f46b53d
Banurekha <banurekha.l@gmail.com>
2017-03-23 14:13:27 -0400

    Initial version  of changing many-to-many editor/work relationship
    Convert has_and_belongs_to_many to has_many works in person and group. Adding with_collaborator module

---
+++ app/repository_models/curation_concern/with_collaborators.rb
@@ -0,0 +1,43 @@
+module CurationConcern
+  module WithCollaborators
+    extend ActiveSupport::Concern
+
+    included do
+      has_and_belongs_to_many :record_editors, class_name: "::Person", property: :has_editor
+      accepts_nested_attributes_for :record_editors, allow_destroy: true, reject_if: :all_blank
+      has_and_belongs_to_many :record_editor_groups, class_name: "::Hydramata::Group", property: :has_editor_group
+      accepts_nested_attributes_for :record_editor_groups, allow_destroy: true, reject_if: :all_blank
+
+      has_and_belongs_to_many :record_viewers, class_name: "::Person", property: :has_viewer
+      accepts_nested_attributes_for :record_viewers, allow_destroy: true, reject_if: :all_blank
+      has_and_belongs_to_many :record_viewer_groups, class_name: "::Hydramata::Group", property: :has_viewer_group
+      accepts_nested_attributes_for :record_viewer_groups, allow_destroy: true, reject_if: :all_blank
+    end
+
+    SOLR_KEY_EDITOR_PIDS = ActiveFedora::SolrService.solr_name(:record_editors).freeze
+    SOLR_KEY_EDITOR_GROUP_PIDS = ActiveFedora::SolrService.solr_name(:record_editor_groups).freeze
+    SOLR_KEY_VIEWER_PIDS = ActiveFedora::SolrService.solr_name(:record_viewers).freeze
+    SOLR_KEY_VIEWER_GROUP_PIDS = ActiveFedora::SolrService.solr_name(:record_viewer_groups).freeze
+
+    # These are the easy record_editor relationships that can be solrized without the full anctics of crawling the relationship graph.
+    #
+    # TODO Curate::EditorIndexingAdapter.write_document_attributes_to_index_layer
+    def to_solr(solr_doc={}, opts={})
+      super(solr_doc, opts)
+      solr_doc[SOLR_KEY_EDITOR_PIDS] = self.record_editor_ids
+      solr_doc[SOLR_KEY_EDITOR_GROUP_PIDS] = self.record_editor_group_ids
+      solr_doc[SOLR_KEY_VIEWER_PIDS] = self.record_viewer_ids
+      solr_doc[SOLR_KEY_VIEWER_GROUP_PIDS] = self.record_viewer_group_ids
+      solr_doc
+    end
+
+
+    def update_index
+      super
+      #TODO
+      Curate.relationship_reindexer.call(self.pid)
+    end
+
+
+  end
+end
