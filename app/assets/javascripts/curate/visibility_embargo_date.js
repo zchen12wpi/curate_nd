6eda639e9ff78ef292bf872961be95ec25b16a01
LaRita Robinson <lrobins5@nd.edu>
2017-12-11 14:38:20 -0500

    Rename javascript functions
    Also found an extraneous function call that hadn't gotten removed when
    the function was moved from each radio button to the main div.

--- app/assets/javascripts/curate/visibility_embargo_date.js
+++ app/assets/javascripts/curate/visibility_embargo_date.js
@@ -1,5 +1,5 @@
 // require embargo release date for only embargo option
-function dateRequired(source_name, object_name) {
+function embargoDateRequired(source_name, object_name) {
   if(source_name === "visibility_embargo"){
     $('[id$=' + object_name + '_embargo_release_date]').attr('required', 'true')
   }else{
@@ -8,7 +8,7 @@ function dateRequired(source_name, object_name) {
 }
 
 // clear the embargo release date value for open access
-function validate(form) {
+function validateCurationConcernForm(form) {
     var is_open = $('#visibility_open')[0]
     var embargo_release_date = $('#' + form.dataset.modelName + '_embargo_release_date')[0]
     if(is_open.checked && embargo_release_date){
