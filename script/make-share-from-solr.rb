b629d8b636a2c26c51323235d8b2b0ef0dbfc8e5
Don Brower <dbrower@nd.edu>
2018-01-31 14:48:56 -0500

    Have make-share-from-solr save results as CSV

--- script/make-share-from-solr.rb
+++ script/make-share-from-solr.rb
@@ -35,6 +35,7 @@
 #
 #     env SOLR_URL="https://solr41prod.example.com:8443/solr/curate" ./make-share-from-solr.rb
 
+require "csv"
 require "date"
 require "rsolr"
 
@@ -47,10 +48,8 @@ class SolrToShare
   attr_reader :target_share_csv
 
   def initialize
-
     date_stamp = Date.today.strftime("%Y-%m-%d")
-    target_share_json = "curate-for-share-#{date_stamp}.json"
-    target_share_csv = "curate-for-share-#{date_stamp}.csv"
+    @target_share_csv = "curate-for-share-#{date_stamp}.csv"
   end
 
 
@@ -71,45 +70,60 @@ class SolrToShare
     record
   end
 
+  def make_string(s)
+    if s.respond_to?('join')
+      s = s.join('|')
+    end
+    if s.respond_to?('gsub')
+      s = s.gsub(/[\n\r]/, ' ')
+    end
+    return s
+  end
 
   def run
     puts "Using SOLR_URL=#{SOLR_URL}"
     puts "Using PID_NAMESPACE=#{PID_NAMESPACE}"
 
+    fields = [
+            "id",
+            "system_create_dtsi",
+            "system_modified_dtsi",
+            "object_state_ssi",
+            "active_fedora_model_ssi",
+            "human_readable_type_tesim",
+            "depositor_tesim",
+            "read_access_group_ssim",
+            "desc_metadata__title_tesim",
+            "desc_metadata__creator_tesim",
+            "desc_metadata__abstract_tesim",
+            "desc_metadata__description_tesim",
+            "desc_metadata__identifier_tesim"
+          ]
     solr = RSolr.connect url: SOLR_URL
 
-    offset = 0
-    loop do
-      puts "Fetching 1000 at offset #{offset}..."
-      response = solr.get 'select', params: {
-        q: "*:*",
-        start: offset,
-        rows: ROW_COUNT,
-        fl: [
-          "id",
-          "system_create_dtsi",
-          "system_modified_dtsi",
-          "object_state_ssi",
-          "active_fedora_model_ssi",
-          "human_readable_type_tesim",
-          "depositor_tesim",
-          "read_access_group_ssim",
-          "desc_metadata__title_tesim",
-          "desc_metadata__creator_tesim",
-          "desc_metadata__abstract_tesim",
-          "desc_metadata__description_tesim",
-          "desc_metadata__identifier_tesim"
-        ]
-      }
-      docs = response["response"]["docs"]
-      docs.each do |record|
-        new_record = process_record(record)
-        if !new_record.nil?
-          puts new_record.to_json
+    puts "opening #{self.target_share_csv}"
+    CSV.open(self.target_share_csv, "wb:UTF-8") do |csv|
+      csv << fields
+      offset = 0
+      loop do
+        puts "Fetching 1000 at offset #{offset}..."
+        response = solr.get 'select', params: {
+          q: "*:*",
+          start: offset,
+          rows: ROW_COUNT,
+          fl: fields
+        }
+        docs = response["response"]["docs"]
+        docs.each do |record|
+          new_record = process_record(record)
+          unless new_record.nil?
+            row = fields.map { |name| make_string(new_record[name]) }
+            csv << row
+          end
         end
+        break if docs.length < ROW_COUNT
+        offset += ROW_COUNT
       end
-      break if docs.length < ROW_COUNT
-      offset += ROW_COUNT
     end
   end
 end
