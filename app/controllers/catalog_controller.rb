c6696f682c99810c1a86f5852e3c4929eba3a66c
Justin Gondron <jgondron@nd.edu>
2018-05-03 15:31:43 -0400

    DLTP-1258: Add additional derived create dates
    - Expanded the derived created date on all models to also convert publication_date and date_issued when they are available, using the priority sequence of publication_date -> date_issued -> date_created.
    - Added some additional attributes to the dev seed data in order to easily test this derived date.
    - Fixed a problem in catalog_controller with the sort field name. Since we're using solr_name helper to determine the full field name, don't need to include the dtsim suffix. Also changed to stored_searchable to add the m to the suffix (to match the way the field is created in the model)
    - Fixed a problem in catalog_controller with the sort function. It must use the ordinal function since it's multivalued. If this because inconsistent or problematic (ex: Solr recommends not using ord() because of high memory usage), then we'll need to modify the schema to be single valued and reindex everything.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -237,7 +237,7 @@ class CatalogController < ApplicationController
   end
 
   def self.created_field
-    solr_name('date_created_derived_dtsim', :stored_sortable , type: :date)
+    solr_name('date_created_derived', :stored_searchable , type: :date)
   end
 
   def self.search_config
@@ -596,8 +596,8 @@ class CatalogController < ApplicationController
     config.add_sort_field "#{uploaded_field} asc", label: "Sort by oldest upload"
     config.add_sort_field "#{modified_field} desc", label: "Sort by newest modification"
     config.add_sort_field "#{modified_field} asc", label: "Sort by oldest modification"
-    config.add_sort_field "#{created_field} desc", label: "Sort by newest date created"
-    config.add_sort_field "#{created_field} asc", label: "Sort by oldest date created"
+    config.add_sort_field "ord(#{created_field}) desc", label: "Sort by newest date created"
+    config.add_sort_field "ord(#{created_field}) asc", label: "Sort by oldest date created"
 
 
     # If there are more than this many search results, no spelling ("did you
