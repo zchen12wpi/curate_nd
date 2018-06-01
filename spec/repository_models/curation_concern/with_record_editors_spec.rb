90b0fd060c7dc582ddedc4ab01cac40a80384960
jmille79 <jmille79@nd.edu>
2018-06-01 13:30:04 -0400

    Applied Suggested Updates

--- spec/repository_models/curation_concern/with_record_editors_spec.rb
+++ spec/repository_models/curation_concern/with_record_editors_spec.rb
@@ -34,37 +34,3 @@ describe CurationConcern::WithRecordEditors do
     end
   end
 end
-
-describe CurationConcern::WithRecordViewers do
-
-  describe 'add_record_viewer_group' do
-    it "raise error for wrong parameter" do
-      user = FactoryGirl.create(:user)
-      work = FactoryGirl.create(:generic_work)
-      expect{work.add_record_viewer_group(user)}.to raise_error
-    end
-
-    it "will be part of the read group and not the edit group" do
-      group = FactoryGirl.create(:group)
-      work = FactoryGirl.create(:generic_work)
-      work.add_record_viewer_group(group)
-      expect(work.edit_groups).not_to include(group.pid)
-
-      expect(work.read_groups).to include(group.pid)
-
-      # => expect(group.view_works).to include(work) does not pass for unknown reason?
-      # => shows as an Array in debug
-      expect(group.view_works.to_a).to include(work)
-    end
-  end
-
-  describe 'remove_record_editor_group' do
-    it "will be removed from the edit group" do
-      group = FactoryGirl.create(:group)
-      work = FactoryGirl.create(:generic_work)
-      work.add_record_viewer_group(group)
-      work.remove_record_viewer_group(group)
-      expect(work.read_groups).not_to include(group.pid)
-    end
-  end
-end
---
+++ spec/repository_models/curation_concern/with_record_viewers_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'active_fedora/test_support'
+
+describe CurationConcern::WithRecordViewers do
+
+  describe 'add_record_viewer_group' do
+    it "raise error for wrong parameter" do
+      user = FactoryGirl.create(:user)
+      work = FactoryGirl.create(:generic_work)
+      expect{work.add_record_viewer_group(user)}.to raise_error
+    end
+
+    it "will be part of the read group and not the edit group" do
+      group = FactoryGirl.create(:group)
+      work = FactoryGirl.create(:generic_work)
+      work.add_record_viewer_group(group)
+      expect(work.edit_groups).not_to include(group.pid)
+
+      expect(work.read_groups).to include(group.pid)
+
+      # => expect(group.view_works).to include(work) does not pass for unknown reason?
+      # => shows as an Array in debug
+      expect(group.view_works.to_a).to include(work)
+    end
+  end
+
+  describe 'remove_record_editor_group' do
+    it "will be removed from the edit group" do
+      group = FactoryGirl.create(:group)
+      work = FactoryGirl.create(:generic_work)
+      work.add_record_viewer_group(group)
+      work.remove_record_viewer_group(group)
+      expect(work.read_groups).not_to include(group.pid)
+    end
+  end
+end
