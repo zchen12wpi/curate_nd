e0ef04b2ee7afe80f8cd84289a1b228a40eb91f1
jmille79 <jmille79@nd.edu>
2018-06-01 13:12:11 -0400

    Repaired Spec

--- spec/workers/group_editor_pids_worker_spec.rb
+++ spec/workers/group_editor_pids_worker_spec.rb
@@ -1,31 +1,29 @@
-
 require 'spec_helper'
 require 'group_editor_pids_worker.rb'
 
 describe GroupEditorPIDsWorker do
   describe '#batch_assign_editor' do
-    valid_pids = ["pid:1", "pid:2", "pid:3", "pid:123"]
+    let(:group) {FactoryGirl.create(:group)}
+    let(:work) {FactoryGirl.create(:generic_work)}
 
-    it 'Assign editor_pid to each pid' do
-      group = FactoryGirl.create(:group)
-      work = FactoryGirl.create(:generic_work)
+    fit 'Assign editor_pid to each pid' do
       editor_pid = group.pid
       pids = [work.pid]
       worker = GroupEditorPIDsWorker.new(editor_pid,pids)
       worker.run
-      work = work.class.find(work.pid)
+      work.reload
       expect(work.edit_groups).to include(group.pid)
     end
 
     it 'Throws exception for Invalid editor_pid' do
       editor_pid = "zxw"
-      pids = ["pid:1", "pid:2", "pid:3"]
+      pids = [work.pid]
       worker = GroupEditorPIDsWorker.new(editor_pid,pids)
       expect{worker.run}.to raise_error(ActiveFedora::ObjectNotFoundError)
     end
 
     it 'Throws exception for Invalid pid in pids list' do
-      editor_pid = "pad:123"
+      editor_pid = group.pid
       pids = ["pid:1", "pid:2", "pib"]
       worker = GroupEditorPIDsWorker.new(editor_pid,pids)
       expect{worker.run}.to raise_error(ActiveFedora::ObjectNotFoundError)
@@ -39,7 +37,7 @@ describe GroupEditorPIDsWorker do
     end
 
     it 'Throws exception for Missing pids' do
-      editor_pid = "pid:123"
+      editor_pid = group.pid
       pids = []
       worker = GroupEditorPIDsWorker.new(editor_pid,pids)
       expect{worker.run}.to raise_error("PIDs is required")
