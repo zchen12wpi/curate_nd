9d5378ae1eede3ae5cf6f45abd2096c2e2a393b4
jmille79 <jmille79@nd.edu>
2018-07-10 10:29:51 -0400

    Applied suggested updates
    Applied Suggestions
    
    Extra adjustment
    
    Suggested Fix

--- script/update_etd_fields.rb
+++ script/update_etd_fields.rb
@@ -1,7 +1,14 @@
 require 'json'
+require 'logger'
+# Start a log
+@logger = Logger.new(STDOUT)
+date_time = Time.now.strftime("%Y_%m_%d_%H_%M_%S")
+@logger = Logger.new("etd_update_#{date_time}.log")
+@logger.level = Logger::DEBUG
+@logger
 # Make sure a file is given
 if ARGV.length < 1
-  puts "ERROR: No File listed in command
+@logger.info "ERROR: No File listed in command
   usage: update_empty_etd_fields.rb update_info.json
 
   json example:
@@ -16,8 +23,8 @@ if ARGV.length < 1
   exit 2
 end
 # Read JSON file
-puts 'Reading JSON File'
 filename = ARGV[0]
+@logger.info "Reading JSON File #{filename}"
 file_content = File.read(filename)
 data = JSON.parse(file_content)
 
@@ -30,38 +37,38 @@ data = JSON.parse(file_content)
 
 data.each do |record|
 # Check if the record exists in CurateND
-  puts "checking record #{record['work_id']}"
-  # next puts "no record found, continuing to next record" unless Etd.exists?(record['work_id'])
+  @logger.info "checking record #{record['work_id']}"
+  # next @logger.info "no record found, continuing to next record" unless Etd.exists?(record['work_id'])
   begin
     etd = Etd.find('und:'+record['work_id'])
   rescue
-    next puts "no record found, continuing to next record"
+    next @logger.info "no record found, continuing to next record"
   end
   record_updated = false
 # Check if the record has the attributes set
-  unless record['catalog_system_number'].nil?
+  if record['catalog_system_number'].present? && etd.alephIdentifier.blank?
     etd.alephIdentifier = record['catalog_system_number']
     record_updated = true
-    puts "new record found"
+    @logger.info "ALEPH ID #{etd.alephIdentifier} found for #{record['work_id']}"
   end
-  unless record['ETD_REVIEWER_SIGNOFF_DATE'].nil?
+  if record['ETD_REVIEWER_SIGNOFF_DATE'].present? && etd.date_approved.blank?
     etd.date_approved = record['ETD_REVIEWER_SIGNOFF_DATE']
     record_updated = true
-    puts "new record found"
+    @logger.info "Signoff Date #{etd.date_approved} found for #{record['work_id']}"
   end
-  unless record['oclc_number'].nil?
+  if record['oclc_number'].present? && etd.urn.blank?
     etd.urn = record['oclc_number']
     record_updated = true
-    puts "new record found"
+    @logger.info "URN #{etd.urn} found found for #{record['work_id']}"
   end
 # Save etd if any records updated
   if record_updated
     begin
-      puts "saving record"
+      @logger.info "saving record #{record['work_id']}"
       etd.save!
-      puts "record saved"
+      @logger.info "record #{record['work_id']} saved"
     rescue
-      puts "record #{record['work_id']} failed to save"
+      @logger.info "record #{record['work_id']} failed to save"
     end
   end
 end
