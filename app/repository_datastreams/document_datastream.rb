e0fe8e8d3ad6cff8adf31dbff1a3952d4dc9e014
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2018-08-16 15:20:02 -0400

    Addressing load issue in Document
    Adding an explicit load for the RDF::ND module. In development,
    I encountered the following error:
    
    ```console
    NameError: uninitialized constant RDF::ND
    ```
    
    In production this manifested in the Document class not having
    the correct predicates in it's inspect. If Document were the first
    of the repository models loaded, then the class would say something
    like:
    
    ```console
    Document.new.title
    NoMethodError <#Document> does not respond to :title
    ```
    
    If Document were loaded after other repository models (e.g Image), then
    we wouldn't encounter the error, and the Document would have the
    expected attributes.
    
    For workers, since usually load one model at a time, they would always
    encounter this error. But for web-application facing, they would not,
    as Document is one amongst many objects that could be loaded.
    
    DLTP-1520

--- app/repository_datastreams/document_datastream.rb
+++ app/repository_datastreams/document_datastream.rb
@@ -1,4 +1,5 @@
 require File.expand_path('../../../lib/rdf/qualified_dc', __FILE__)
+require File.expand_path('../../../lib/rdf/nd', __FILE__)
 
 class DocumentDatastream < ActiveFedora::NtriplesRDFDatastream
   map_predicates do |map|
