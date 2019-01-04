d41acd434339401fbc91acda3ab09e7c18acd40f
LaRita Robinson <lrobins5@nd.edu>
2019-01-04 17:21:48 -0500

    Make access requests configurable
    Allows for more than just ETD's to show the access request option, based
    on content of access_request_map.yml file. First step toward future
    option to allow access requests based on a metadata field on a work.

--- app/helpers/curation_concern_helper.rb
+++ app/helpers/curation_concern_helper.rb
@@ -17,4 +17,36 @@ module CurationConcernHelper
     markup << %(</ul></td></tr>)
     markup.html_safe
   end
+
+  def build_access_request_for(curation_concern, file)
+    request_subject = "[#{ curation_concern.human_readable_type } Access Request] for #{curation_concern.title} (id: #{curation_concern.noid})"
+    request_body = "I am interested in #{ curation_concern.title }.\nI would like to view one of the withheld files associated with it (id: #{file.noid})."
+    request_html = <<-markup
+      request permission to view this file from #{this_request["access_request_department"]}.
+      <p>
+      <a class="btn btn-default" href="mailto:#{URI.escape(this_request["access_request_recipient"])}?subject=#{URI.escape(request_subject)}&body=#{URI.escape(request_body)}">Request Access</a>
+      </p>
+    markup
+  end
+
+  def access_request_allowed?
+    return false if this_request.nil?
+    return false if request_recipient.nil?
+    return true
+  end
+
+  private
+  def request_recipient
+    return this_request["access_request_recipient"] if this_request["access_request_method"] == "email"
+    # return some metadata field if this_request["access_request_method"] == "metadata"
+    nil
+  end
+
+  def this_request
+    @this_request ||= access_request_data[curation_concern.class.to_s.downcase]
+  end
+
+  def access_request_data
+    @access_request_data ||= YAML.load( File.open( Rails.root.join( 'config/access_request_map.yml' ) ) ).freeze
+  end
 end
