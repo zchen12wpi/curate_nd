54362326301bfd2c140204c8ece98b39570d417b
LaRita Robinson <lrobins5@nd.edu>
2019-04-25 14:55:14 -0400

    Comment out invalid javascript

--- app/assets/javascripts/curate/viewer_build.js
+++ app/assets/javascripts/curate/viewer_build.js
@@ -1,13 +1,16 @@
 $(function(url){
   'use strict';
+  // the commented out code causes the app to crash.
+  // Error: ActionView::Template::Error - Unexpected token name «request», expected punc «,». To use ES6 syntax, harmony mode must be enabled with Uglifier.new(:harmony => true).
 
-  var request = false;
-  if(window.XMLHttpRequest)
-    request = new XMLHttpRequest();
-  else
-    request = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  request.open('GET', url);
-  if (request.status === 200) { return true; }
+
+  // var request = false;
+  // if(window.XMLHttpRequest)
+  //   request = new XMLHttpRequest();
+  // else
+  //   request = new ActiveXObject("Microsoft.XMLHTTP");
+  // }
+  // request.open('GET', url);
+  // if (request.status === 200) { return true; }
   return false;
 })
