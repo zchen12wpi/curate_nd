30386e15cb7e139184acc6cd942d0af053686d02
msuhovec <suhovecky@nd.edu>
2019-05-03 14:30:26 -0400

    stub route and skeleton app/controller and view files

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,36 @@
+class Api::ItemsController < ApplicationController
+  with_themed_layout('1_column')
+
+  # GET /api/items
+  def index
+  end
+
+  # GET /api/items/1
+  def show
+  end
+
+  # GET /api/items/new
+  def new
+  end
+
+  # GET /api/items/1/edit
+  def edit
+  end
+
+  # POST /api/items
+  def create
+  end
+
+  # PATCH/PUT /api/items/1
+  def update
+  end
+
+  # DELETE /api/items/1
+  def destroy
+  end
+
+  def dismiss
+  end
+
+  private
+end
