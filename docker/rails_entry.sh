c51a5dfbacf100ec55020ebfd650a2ff21ac0353
Justin Gondron <jgondron@nd.edu>
2019-05-06 11:39:08 -0400

    Experiment to use puma in dev

--- docker/rails_entry.sh
+++ docker/rails_entry.sh
@@ -7,4 +7,5 @@ bundle exec rake db:schema:load
 # If we find people are ok with just starting a shell in the container and running the db create/load before running specs, then we can remove this
 RAILS_ENV=test bundle exec rake db:create db:schema:load
 
-exec bundle exec thin start -p 3000 --ssl --ssl-key-file dev_server_keys/server.key --ssl-cert-file dev_server_keys/server.crt
+exec bundle exec puma -b "ssl://0.0.0.0:3000?key=dev_server_keys/server.key&cert=dev_server_keys/server.crt"
+#exec bundle exec thin start -p 3000 --ssl --ssl-key-file dev_server_keys/server.key --ssl-cert-file dev_server_keys/server.crt
