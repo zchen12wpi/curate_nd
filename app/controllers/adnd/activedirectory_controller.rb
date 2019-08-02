36600bdb84b4dabc705c9c52929d3ec8cc25bb2e
Mark Suhovecky <msuhovec@nd.edu>
2019-08-02 09:13:30 -0400

    pushing 1st coffeescript attempt

--- app/controllers/adnd/activedirectory_controller.rb
+++ app/controllers/adnd/activedirectory_controller.rb
@@ -4,11 +4,11 @@ require 'net/http'
 # use library api to query active directory
 class Adnd::ActivedirectoryController < ApplicationController
 
-  # GET /adnd/namelist/:id
+  # GET /adnd/namelist?q=:id
   def namelist
-    adnd_url_base = ENV.fetch['AD_API_URL']
-    adnd_token = ENV.fetch['AD_API_TOKEN]
-    url = URI("#{'adnd_url_base'}#{params[:id]}.json?auth_token=#{adnd_token}")
+    adnd_url_base = ENV.fetch('AD_API_URL')
+    adnd_token = ENV.fetch('AD_API_TOKEN')
+    url = URI("#{adnd_url_base}#{params[:q]}.json?auth_token=#{adnd_token}")
 
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
