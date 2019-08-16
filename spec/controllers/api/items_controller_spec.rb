68a1d3372338223c93aacb207b464af0e9eb773c
LaRita Robinson <lrobins5@nd.edu>
2019-08-16 13:56:18 -0400

    Rename initiate_trx to trx_initiate
    Change items_controller_spec to match change in controller.

--- spec/controllers/api/items_controller_spec.rb
+++ spec/controllers/api/items_controller_spec.rb
@@ -144,12 +144,12 @@ describe Api::ItemsController do
     end
   end
 
-  describe '#initiate_trx' do
+  describe '#trx_initiate' do
     context 'with api token which grants access' do
       it 'returns 200 and json document' do
         request.headers['X-Api-Token'] = token.sha
         request.headers['HTTP_ACCEPT'] = "application/json"
-        get :initiate_trx
+        get :trx_initiate
         expect(response).to be_successful
       end
     end
@@ -157,7 +157,7 @@ describe Api::ItemsController do
     context 'without api token which grants access' do
       it 'returns 417 and json document' do
         request.headers['HTTP_ACCEPT'] = "application/json"
-        get :initiate_trx
+        get :trx_initiate
         expect(response.status).to eq(417) #expectation_failed
       end
     end
