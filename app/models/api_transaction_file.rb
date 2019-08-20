e541f9f026bb997fdad423006d82f9e64cd1fed3
LaRita Robinson <lrobins5@nd.edu>
2019-08-20 17:17:04 -0400

    Fixes to update trx file

--- app/models/api_transaction_file.rb
+++ app/models/api_transaction_file.rb
@@ -1,12 +1,14 @@
 class ApiTransactionFile < ActiveRecord::Base
   self.table_name = 'api_transaction_files'
   belongs_to :api_transaction, foreign_key: 'trx_id'
-  self.primary_keys = :trx_id, :file_id, :file_seq_nbr
   validates :file_seq_nbr, uniqueness: {scope: [:trx_id, :file_id]}
 
   def self.next_seq_nbr(trx_id:, file_id:)
-    find_file = ApiTransactionFile.find(trx_id: trx_id, file_id: file_id).last
-    return 1 unless find_file.exists?
+    begin
+      find_file = ApiTransactionFile.where(trx_id: trx_id, file_id: file_id).order(file_seq_nbr: :desc).first
+    rescue ActiveRecord::RecordNotFound
+      return 1
+    end
     1 + find_file.file_seq_nbr
   end
 end
