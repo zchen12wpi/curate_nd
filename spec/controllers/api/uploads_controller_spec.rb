c41da175f01807ea35398790553353a74323b5fc
LaRita Robinson <lrobins5@nd.edu>
2019-08-21 11:39:16 -0400

    Modify spec for new trx method changes

--- spec/controllers/api/uploads_controller_spec.rb
+++ spec/controllers/api/uploads_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Api::UploadsController do
   let(:user) { FactoryGirl.create(:user) }
   let(:token) { ApiAccessToken.create(issued_by: user.id, user: user) }
+  let(:transaction) { double }
 
   describe '#trx_initiate' do
     context 'with api token which grants access' do
@@ -14,11 +15,25 @@ describe Api::UploadsController do
       end
     end
 
-    context 'without api token which grants access' do
+    context 'when save fails' do
+      before do
+        allow(ApiTransaction).to receive(:new).and_return(:transaction)
+        allow(transaction).to receive(:save).and_return false
+      end
+
       it 'returns 417 and json document' do
+        request.headers['X-Api-Token'] = token.sha
         request.headers['HTTP_ACCEPT'] = "application/json"
         get :trx_initiate
-        expect(response.status).to eq(417) #expectation_failed
+        expect(response).to be_successful
+      end
+    end
+
+    context 'without api token which grants access' do
+      it 'returns 401 and json document' do
+        request.headers['HTTP_ACCEPT'] = "application/json"
+        get :trx_initiate
+        expect(response.status).to eq(401) #expectation_failed
       end
     end
   end
