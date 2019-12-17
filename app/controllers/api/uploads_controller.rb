e57bd4516c3a016b8dbe14c2d3acb0dad3476bf0
Mark Suhovecky <msuhovec@nd.edu>
2019-12-17 09:53:51 -0500

    pass trx_id to start_api_ingest differently
    trx_id vs trx_id: trx_id

--- app/controllers/api/uploads_controller.rb
+++ app/controllers/api/uploads_controller.rb
@@ -127,7 +127,7 @@ class Api::UploadsController < Api::BaseController
       # update trx status
       ApiTransaction.set_status_based_on(trx_id: trx_id, action: :commit)
 
-      BatchIngestor.start_api_ingest( trx_id: trx_id )
+      BatchIngestor.start_api_ingest( trx_id )
 
       # respond ok
       render json: { trx_id: trx_id }, status: :ok
