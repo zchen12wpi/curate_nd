c1eafb40eea680ddcb1f7b64c84b6cd2cd0713e5
Mark Suhovecky <msuhovec@nd.edu>
2020-01-24 11:36:20 -0500

    added context data + rubocop changes

--- app/services/api/file_metadata_formatter.rb
+++ app/services/api/file_metadata_formatter.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module Api
+  # FileMetadataFormatter creates default metadata for uplaofed Generic Files
   class FileMetadataFormatter
     attr_reader :work_id, :file_id, :file_name, :remaining_content, :user_id
 
@@ -15,19 +18,25 @@ module Api
       return false unless file_id
       return false unless file_name
       return false unless user_id
+
       true
     end
 
     def initial_metadata
       metadata_hash = remaining_content
+      metadata_hash['@context'] = {} unless metadata_hash['@context']
+      metadata_hash['@context']['frels'] = 'info:fedora/fedora-system:def/relations-external#'
+      metadata_hash['@context']['dc'] =  'http://purl.org/dc/terms/'
+      metadata_hash['@context']['nd'] =  'https://library.nd.edu/ns/terms/'
+      metadata_hash['@context']['und'] = 'https://curate.nd.edu/show/'
       metadata_hash['@id'] = "und:#{file_id}"
-      metadata_hash['nd:filename'] = "#{file_name}"
-      metadata_hash['dc:title'] = "#{file_name}"
-      metadata_hash['nd:afmodel'] = "GenericFile"
-      metadata_hash['nd:accessReadGroup'] = "private"
+      metadata_hash['nd:filename'] = file_name.to_s
+      metadata_hash['dc:title'] = file_name.to_s
+      metadata_hash['nd:afmodel'] = 'GenericFile'
+      metadata_hash['nd:accessReadGroup'] = 'private'
       metadata_hash['frels:isPartOf'] = "und:#{work_id}"
-      metadata_hash['nd:owner'] = "#{user_id}"
-      metadata_hash['nd:accessEdit'] = "#{user_id}"
+      metadata_hash['nd:owner'] = user_id.to_s
+      metadata_hash['nd:accessEdit'] = user_id.to_s
       JSON.dump(metadata_hash)
     end
   end
