2a16401bebf553ce1369176ca92b91fdecddbf48
LaRita Robinson <lrobins5@nd.edu>
2020-02-25 13:01:39 -0500

    Remove service_url from Bendo presenter

--- app/services/bendo/datastream_presenter.rb
+++ app/services/bendo/datastream_presenter.rb
@@ -4,7 +4,7 @@ module Bendo
   class DatastreamPresenter
     attr_reader :datastream
 
-    def initialize(datastream:, service_url: Bendo.url)
+    def initialize(datastream:)
       @datastream = datastream
     end
 
